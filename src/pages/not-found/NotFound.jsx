a2b4bdea653185c57e8f86b27bce533d5e3ade73
Rominos7 <Rominos7@gmail.com>
2025-05-04 21:24:03 +0300

    add NotFound page

---
+++ src/pages/not-found/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NotFound = () => {
+    return(
+        <div class='not-found-page-container'>
+            <h1>Page was not found</h1>
+        </div>
+    )
+};

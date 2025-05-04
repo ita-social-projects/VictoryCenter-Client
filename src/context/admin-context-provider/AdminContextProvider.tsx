9d27de5044c8e3fa90eaba36170197559b797c34
Rominos7 <Rominos7@gmail.com>
2025-05-04 23:19:13 +0300

    create AdminContextProvider

---
+++ src/context/admin-context-provider/AdminContextProvider.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+};
+
+type ContextType = {
+  token: string;
+};
+
+const AdminContext = createContext<ContextType | undefined>(undefined);
+
+export const AdminContextProvider = ({ children }: Props) => {
+  // DEV NOTE:
+  // here you need to call login method to init login proccess
+  // for example
+  /* 
+        const isLoginSuccessful =  initLogin();
+        // additional logic to show log in page if needed
+
+    */
+  // if isLoginSuccessful = true then we will "leed" user to admin page
+  // if isLoginSuccessful = false then we will show user error message
+
+  const isLoginSuccessful = true;
+
+  return isLoginSuccessful ? (
+    <AdminContext.Provider
+      value={{
+        token: "fake-token", // token value goes here
+      }}
+    >
+      {children}
+    </AdminContext.Provider>
+  ) : (
+    <div className="error-message-container">
+      <p>YOU SHALL NOT PASS!!!</p>
+      <img src={"https://i.gifer.com/36Ja.gif"} alt="you shall not pass!" />
+    </div>
+  );
+};
+
+export const useAdminContext = () => useContext(AdminContext) as ContextType;

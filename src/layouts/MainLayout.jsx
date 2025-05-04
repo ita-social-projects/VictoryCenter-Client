9ec8c74bb398460191785099e21793fae72c4726
Rominos7 <Rominos7@gmail.com>
2025-05-04 21:26:34 +0300

    add main layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import { Navigation } from '../components/navigation/Navigation';
+
+export const MainLayout = () => (
+  <div>
+    <div className="navigation-container">
+      <Navigation />
+    </div>
+    <div className="page-container">
+      <Outlet />
+    </div>
+  </div>
+);

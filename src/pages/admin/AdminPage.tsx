b14e920520956e812cd3217f5673cb4d247b1dba
Rominos7 <Rominos7@gmail.com>
2025-05-04 21:24:35 +0300

    add admin page

---
+++ src/pages/admin/AdminPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AdminPage = () => {
+    return(
+        <div className='admin-page-container'>
+            <h1>Admin page</h1>
+        </div>
+    )
+};

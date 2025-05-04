67fb05e7787b0f9e9857cad1bfbc38d007d86fd8
Rominos7 <Rominos7@gmail.com>
2025-05-04 23:49:22 +0300

    move MainLayout to separate folder

--- src/layouts/main-layout/MainLayout.jsx
+++ src/layouts/main-layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import { Navigation } from '../components/navigation/Navigation';
+import { Navigation } from '../../components/navigation/Navigation';
 
 export const MainLayout = () => (
   <div>

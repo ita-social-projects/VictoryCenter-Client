fb6e5bb3c6c56d4921d001172741beb90540ae03
Rominos7 <Rominos7@gmail.com>
2025-05-04 23:49:03 +0300

    move AppRouter to separate folder

--- src/routes/app-router/AppRouter.jsx
+++ src/routes/app-router/AppRouter.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainLayout } from '../layouts/MainLayout';
+import { MainLayout } from '../../layouts/main-layout/MainLayout';
 
-import { AdminPage } from '../pages/admin/AdminPage';
+import { AdminPage } from '../../pages/admin/AdminPage';
 
-import { HomePage } from '../pages/user-pages/home/HomePage';
-import { Page1 } from '../pages/user-pages/page1/Page1';
-import { Page2 } from '../pages/user-pages/page2/Page2';
+import { HomePage } from '../../pages/user-pages/home/HomePage';
+import { Page1 } from '../../pages/user-pages/page1/Page1';
+import { Page2 } from '../../pages/user-pages/page2/Page2';
 
-import { NotFound } from '../pages/not-found/NotFound';
+import { NotFound } from '../../pages/not-found/NotFound';
 
-import { routes } from '../const/routes';
+import { routes } from '../../const/routes';
 
 const { adminRoute, userPageRoutes: { page1Route, page2Route } } = routes;
 

80206cc06c1a5b4932dc73d510fed7c56322b9e1
Rominos7 <Rominos7@gmail.com>
2025-05-04 21:25:56 +0300

    add routes const

---
+++ src/const/routes.ts
@@ -0,0 +1,15 @@
+export const adminRoute = '/admin-page';
+
+// user pages routes
+export const page1Route = '/page-1';
+export const page2Route = '/page-2';
+
+export const userPageRoutes = {
+    page1Route,
+    page2Route
+};
+
+export const routes = {
+    adminRoute,
+    userPageRoutes
+};

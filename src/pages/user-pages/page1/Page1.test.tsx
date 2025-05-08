2bb2e2b1054601cb9b0bd789badd1633ddbd63f5
Rominos7 <Rominos7@gmail.com>
2025-05-08 22:56:19 +0300

    add test for Page 1

---
+++ src/pages/user-pages/page1/Page1.test.tsx
@@ -0,0 +1,41 @@
+import { act } from 'react';
+import { render } from '@testing-library/react';
+import { Page1 } from './Page1';
+import * as Page1DataFetchModule from '../../../services/data-fetch/user-pages-data-fetch/page-1-data-fetch/page1DataFetch';
+
+const spyPage1DataFetch = jest.spyOn(Page1DataFetchModule, 'page1DataFetch');
+
+describe('Page1', () => {
+  const mockHeader = 'Test Header';
+  const mockContent = 'Test Content';
+
+  beforeEach(() => {
+    spyPage1DataFetch.mockResolvedValue({
+      header: mockHeader,
+      content: mockContent,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders the component', async () => {
+    const { container } = render(<Page1 />);
+    
+    await act(() => {
+        container
+    });
+
+    const header = container.querySelector('.header');
+    const content = container.querySelector('.content');
+
+    expect(header).toBeInTheDocument();
+    expect(header?.textContent).toEqual(mockHeader);
+
+    expect(content).toBeInTheDocument();
+    expect(content?.textContent).toEqual(mockContent);
+
+    expect(spyPage1DataFetch).toHaveBeenCalledTimes(1);
+  });
+});
--- src/pages/user-pages/page1/Page1.tsx
+++ src/pages/user-pages/page1/Page1.tsx
@@ -25,8 +25,8 @@ export const Page1 = () => {
 
     return (
       <div className="page1-container">
-        <h1>{headerInfo}</h1>
-        <p>{contentInfo}</p>
+        <h1 className='header'>{headerInfo}</h1>
+        <p className='content'>{contentInfo}</p>
       </div>
     );
 };

9b9de72a6efdd6d0e4ff99135850773d3b2944b9
Rominos7 <Rominos7@gmail.com>
2025-05-08 23:52:58 +0300

    new comment

--- src/pages/not-found/NotFound.test.tsx
+++ src/pages/not-found/NotFound.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { NotFound } from './NotFound';
 
+// new comment
+
 describe('NotFound', () => {
   it('renders the component', () => {
     const { container } = render(<NotFound />);

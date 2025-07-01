809ccae7fb2fd2d0cc07384d9197b6dae648831f
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-07-01 19:03:14 +0300

    Add Dtos for teamPageDataFetch

--- src/types/TeamPage.ts
+++ src/types/TeamPage.ts
@@ -10,6 +10,24 @@ export interface TeamItem {
     description: string;
     members: Member[];
 }
+
+export interface PublicCategoryWithTeamMembersDto {
+    id: number;
+    categoryName: string;
+    description: string | null;
+    teamMembers: PublicTeamMemberDto[];
+}
+
+export interface PublicTeamMemberDto {
+    id: number;
+    fullName: string;
+    description: string | null;
+}
+
+export interface TeamPageData {
+  teamData: TeamItem[];
+}
+
 export interface TeamMemberProps {
     member: Member;
 }

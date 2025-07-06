810b004d46075f7454452fc4e2229d5866baec9c
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-07-06 16:42:04 +0300

    Proxy hotfix

---
+++ src/setupProxy.ts
@@ -0,0 +1,17 @@
+import { Application } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { getEnvVariable } from './utils/functions/getEnvVariable';
+
+const backendUrl = getEnvVariable('REACT_APP_BACKEND_URL');
+const targetUrl = getEnvVariable('REACT_APP_PROXY_TARGET');
+
+module.exports = (app: Application) => {
+    app.use(
+        backendUrl,
+        createProxyMiddleware({
+            target: targetUrl,
+            changeOrigin: true,
+            secure: false,
+        })
+    );
+};

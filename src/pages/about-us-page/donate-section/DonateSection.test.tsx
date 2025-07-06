1ca2537fbd3393e68b42b74bb2f3ff981287b768
Olyaaaaaaa <mrakaolga@gmail.com>
2025-07-06 16:27:58 +0300

    Fixed sonar_cloud bugs

--- src/pages/about-us-page/donate-section/DonateSection.test.tsx
+++ src/pages/about-us-page/donate-section/DonateSection.test.tsx
@@ -8,30 +8,31 @@ import {
 } from '../../../const/about-us-page/about-us-page';
 
 describe('DonateSection', () => {
-    
-    beforeEach(() => {
-        render(<DonateSection />);
-    });
 
     test('should render section with correct title', () => {
+        render(<DonateSection />);
         expect(screen.getByText(DONATE_TITLE)).toBeInTheDocument();
     });
 
     test('should render subtitle correctly', () => {
+        render(<DonateSection />);
         expect(screen.getByText(DONATE_DETAILS)).toBeInTheDocument();
     });
 
     test('should render donate button', () => {
+        render(<DonateSection />);
         expect(screen.getByText(DONATE)).toBeInTheDocument();
         expect(screen.getByText(DONATE)).toHaveClass('donate-button');
     });
 
     test('should render "become-a-partner" button correctly', () => {
+        render(<DonateSection />);
         expect(screen.getByText(BECOME_PARTNER)).toBeInTheDocument();
         expect(screen.getByText(BECOME_PARTNER)).toHaveClass('partner-button');
     });
 
     test('should render background image with correct className', () => {
+        render(<DonateSection />);
         const image = screen.getByAltText('Background horses');
         expect(image).toHaveClass('donate-background');
         expect(image).toHaveAttribute('src', expect.stringContaining('donate-background'));

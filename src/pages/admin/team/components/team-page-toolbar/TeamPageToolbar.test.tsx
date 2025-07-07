8dbf967de10470fc4e87346aec977eef4f80c1c6
Olyaaaaaaa <mrakaolga@gmail.com>
2025-07-07 15:12:50 +0300

    removed never-used import

--- src/pages/admin/team/components/team-page-toolbar/TeamPageToolbar.test.tsx
+++ src/pages/admin/team/components/team-page-toolbar/TeamPageToolbar.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render, screen, fireEvent, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import {TEAM_CONFIRM} from "../../../../../const/team";
 import {TeamPageToolbar, TeamPageToolbarProps} from './TeamPageToolbar';
 
 jest.mock('../../../../../assets/icons/plus.svg', () => 'plus-icon.svg');

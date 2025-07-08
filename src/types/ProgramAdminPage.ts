882d7280ef63d9b6b6c0863c30a4ed76616054f9
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-07-08 22:57:16 +0300

    Add ProgramCategory and Program types for Admin page

---
+++ src/types/ProgramAdminPage.ts
@@ -0,0 +1,14 @@
+﻿export interface ProgramCategory {
+    id: number;
+    name: string;
+    programsCount: number;
+}
+
+export interface Program {
+    id: number;
+    name: string;
+    description: string;
+    categories: ProgramCategory[];
+    status: string;
+    img: string;
+}

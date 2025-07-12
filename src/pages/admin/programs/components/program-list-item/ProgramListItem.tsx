ae88a027ee02f1db2dc3547993f69d7750d0e5a7
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-07-12 14:09:17 +0300

    Fix ProgramListItem to follow mockup

--- src/pages/admin/programs/components/program-list-item/ProgramListItem.tsx
+++ src/pages/admin/programs/components/program-list-item/ProgramListItem.tsx
@@ -1,62 +1,76 @@
-﻿import BlankImage from '../../../../../assets/images/admin/blank-image.svg'
+﻿import BlankImage from '../../../../../assets/images/admin/blank-image.svg';
 import InfoIcon from '../../../../../assets/icons/info.svg';
-import {Program, ProgramStatus} from '../../../../../types/ProgramAdminPage';
-import {Tooltip} from '../../../../../components/common/tooltip/Tooltip';
+import { Program, ProgramStatus } from '../../../../../types/ProgramAdminPage';
+import { Tooltip } from '../../../../../components/common/tooltip/Tooltip';
 import classNames from 'classnames';
 import './programs-list-item.scss';
 
 export interface ProgramListItemProps {
-    program: Program;
-    handleOnDeleteProgram: (program: Program) => void;
-    handleOnEditProgram: (program: Program) => void;
+  program: Program;
+  handleOnDeleteProgram: (program: Program) => void;
+  handleOnEditProgram: (program: Program) => void;
 }
 
-const statusTypeToText = (status: ProgramStatus) => {
-    return status === "Published" ? "Опубліковано" : "Чернетка";
-}
+const statusTypeToText = (status: ProgramStatus): string => {
+  return status === 'Published' ? 'Опубліковано' : 'Чернетка';
+};
 
 export const ProgramListItem = ({
-                                    program,
-                                    handleOnDeleteProgram,
-                                    handleOnEditProgram
-                                }: ProgramListItemProps) => {
-    return (
-        <div className="programs-wrapper">
-            <div className="programs-identity">
-                <img src={program.img || BlankImage} alt={`${program.name}-img`}/>
-                <p>{program.name}</p>
-            </div>
-            <div className="programs-description">
-                <p>{program.description}</p>
-            </div>
-            <div className="programs-controls">
-                <div className="programs-status-container">
-                    <div className={classNames('programs-status', {
-                        'programs-status-draft': program.status === 'Draft',
-                        'programs-status-published': program.status === 'Published',
-                    })}>
-                        <span>•</span>
-                        <span>{statusTypeToText(program.status)}</span>
-                    </div>
-                </div>
-                <div className="programs-actions">
-                    <div className="programs-actions-spacer"/>
-                    <Tooltip position="bottom">
-                        <Tooltip.Trigger>
-                            <img src={InfoIcon} alt="info" className="tooltip-icon"/>
-                        </Tooltip.Trigger>
-                        <Tooltip.Content>
-                            <div className="programs-actions-tooltip-container">
-                                <b>Опубліковано в:</b>
-                                {program.categories.map(c => <span key={c.id}>{c.name}</span>)}
-                            </div>
-                        </Tooltip.Content>
-                    </Tooltip>
-                    <div className="programs-actions-spacer"/>
-                    <button type="button" onClick={() => handleOnEditProgram(program)} className="programs-actions-edit" />
-                    <button type="button" onClick={() => handleOnDeleteProgram(program)} className="programs-actions-delete" />
-                </div>
-            </div>
+  program,
+  handleOnDeleteProgram,
+  handleOnEditProgram,
+}: ProgramListItemProps) => {
+  return (
+    <div className="program-item">
+      <div className="program-info">
+        <div className="program-info-identity">
+          <img src={program.img || BlankImage} alt={`${program.name}-img`} />
+          <p>{program.name}</p>
         </div>
-    );
-}
+        <div className="program-info-description">
+          <p>{program.description}</p>
+        </div>
+        <div className="program-info-status">
+          <div
+            className={classNames('program-status', {
+              'draft': program.status === 'Draft',
+              'published': program.status === 'Published',
+            })}
+          >
+            <span>•</span>
+            <span>{statusTypeToText(program.status)}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="program-actions">
+        <Tooltip position="bottom">
+          <Tooltip.Trigger>
+            <img src={InfoIcon} alt="info" className="tooltip-icon" />
+          </Tooltip.Trigger>
+          <Tooltip.Content>
+            <div className="program-actions-tooltip">
+              <b>Опубліковано в:</b>
+              {program.categories.map((c) => (
+                <span key={c.id}>{c.name}</span>
+              ))}
+            </div>
+          </Tooltip.Content>
+        </Tooltip>
+
+        <div className="program-actions-buttons">
+          <button
+            type="button"
+            onClick={() => handleOnEditProgram(program)}
+            className="edit-btn"
+          />
+          <button
+            type="button"
+            onClick={() => handleOnDeleteProgram(program)}
+            className="delete-btn"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};

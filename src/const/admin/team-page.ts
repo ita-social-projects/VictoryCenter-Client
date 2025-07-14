6f6b2db51d4c8c8876dc5e46df42dfd99b2501b5
Rominos7 <Rominos7@gmail.com>
2025-07-14 23:14:16 +0300

    export categoryMap

--- src/const/admin/team-page.ts
+++ src/const/admin/team-page.ts
@@ -1,1 +1,9 @@
+import { TeamCategory } from '../../types/TeamPage';
+
 export const TEAM_STATUS_DEFAULT = 'Статус';
+
+export const categoryMap: Record<TeamCategory, number> = {
+    'Основна команда': 1,
+    'Наглядова рада': 2,
+    'Радники': 3,
+};

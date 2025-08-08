ec7b7a35ba5f0ce175d7c814c134f33444d3fd78
Vladimir <sushinskyvladimir@gmail.com>
2025-08-08 11:08:26 +0300

    Fixed lint issues

--- src/utils/hooks/use-admin-client/useAdminClient.ts
+++ src/utils/hooks/use-admin-client/useAdminClient.ts
@@ -1,4 +1,4 @@
-import { useAdminContext } from "../../../contexts/admin/admin-context-provider/AdminContextProvider";
+import { useAdminContext } from '../../../contexts/admin/admin-context-provider/AdminContextProvider';
 
 // This hook is made for components that need to make API calls to the protected endpoints.
 export const useAdminClient = () => useAdminContext().client;

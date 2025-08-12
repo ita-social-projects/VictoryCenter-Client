ba585fb25e45ce6e3723dad909a877d1e264b6bb
pasterik <pomapaster@gmail.com>
2025-08-12 17:04:51 +0300

    Added NotFound page

--- src/pages/public/not-found/notfound-message/NotFoundMessage.test.tsx
+++ src/pages/public/not-found/notfound-message/NotFoundMessage.test.tsx
@@ -1,12 +1,17 @@
 import { render } from '@testing-library/react';
 import { NotFoundMessage } from './NotFoundMessage';
+import { MemoryRouter } from 'react-router';
 
-describe('NotFoundFooter', () => {
+describe('NotFoundMessage', () => {
     it('renders the component', () => {
-        const { container } = render(<NotFoundMessage />);
-        const pageContainer = container.querySelector('.not-found-footer-container');
-        const pageText = container.querySelector('.not-found-footer-text');
-        const pageDescription = container.querySelector('.not-found-footer-description');
+        const { container } = render(
+            <MemoryRouter>
+                <NotFoundMessage />
+            </MemoryRouter>,
+        );
+        const pageContainer = container.querySelector('.not-found-message-container');
+        const pageText = container.querySelector('.not-found-message-text');
+        const pageDescription = container.querySelector('.not-found-message-description');
 
         expect(pageContainer).toBeInTheDocument();
         expect(pageText).toBeInTheDocument();

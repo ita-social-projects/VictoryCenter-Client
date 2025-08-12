8f4cd5cc1e4df5f5e0bfce0410c8f37d91d008e6
pasterik <pomapaster@gmail.com>
2025-08-12 18:02:45 +0300

    Fixed bug

--- src/pages/public/not-found/NotFound.test.tsx
+++ src/pages/public/not-found/NotFound.test.tsx
@@ -1,19 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { NotFound } from './NotFound';
-jest.mock('./notfound-page-intro/NotFoundIntro', () => ({
+jest.mock('./notfound-intro/NotFoundIntro', () => ({
     NotFoundIntro: () => <div data-testid="not-found-page-intro-container">NotFoundIntro</div>,
 }));
 
-jest.mock('./notfound-footer/NotFoundFooter', () => ({
-    NotFoundFooter: () => <div data-testid="not-found-footer-container">NotFoundFooter</div>,
+jest.mock('./notfound-message/NotFoundMessage', () => ({
+    NotFoundMessage: () => <div data-testid="not-found-message-container">NotFoundMessage</div>,
 }));
 
 describe('NotFound', () => {
     it('renders the component', () => {
         const { container } = render(<NotFound />);
         const pageContainer = container.querySelector('.not-found-page-container');
-        expect(screen.getByTestId('intro-section')).toBeInTheDocument();
-        expect(screen.getByTestId('program-section')).toBeInTheDocument();
+        expect(screen.getByTestId('not-found-page-intro-container')).toBeInTheDocument();
+        expect(screen.getByTestId('not-found-message-container')).toBeInTheDocument();
         expect(pageContainer).toBeInTheDocument();
     });
 });

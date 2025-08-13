67c5e211199ca79c85d2a6ea6215e18e647fc300
pasterik <pomapaster@gmail.com>
2025-08-13 14:14:12 +0300

    Fixed bug

--- src/const/public/notfound-page.ts
+++ src/const/public/notfound-page.ts
@@ -1,7 +1,5 @@
 export const ERROR_404 = '404';
-export const ERROR_404_TEXT = 'Сторінку не знайдено';
-export const ERROR_404_GO_BACK = 'Повернутись на головну';
-export const ERROR_404_DESCRIPTION = {
-    FIRST_PART: 'На жаль, за цим посиланням нічого немає.',
-    SECOND_PART: 'Спробуйте почати з головної або звернутись до нашої команди.',
-};
+export const TEXT = 'Сторінку не знайдено';
+export const GO_BACK_BUTTON = 'Повернутись на головну';
+export const DESCRIPTION =
+    'На жаль, за цим посиланням нічого немає. Спробуйте почати з головної або звернутись до нашої команди.';
--- src/pages/public/not-found-page/not-found-message/NotFoundMessage.tsx
+++ src/pages/public/not-found-page/not-found-message/NotFoundMessage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NotFoundMessage.scss';
-import { ERROR_404_DESCRIPTION, ERROR_404_TEXT, ERROR_404_GO_BACK } from '../../../../const/public/notfound-page';
+import { DESCRIPTION, TEXT, GO_BACK_BUTTON } from '../../../../const/public/notfound-page';
 import ArrowIcon from '../../../../assets/icons/arrow-up-right.svg';
 import { PUBLIC_ROUTES } from '../../../../const/public/routes';
 import { NavLink } from 'react-router';
@@ -9,14 +9,13 @@ export const NotFoundMessage = () => {
     return (
         <div className="not-found-message-container">
             <div className="not-found-message-text">
-                <h1>{ERROR_404_TEXT}</h1>
+                <h1>{TEXT}</h1>
             </div>
             <div className="not-found-message-description">
-                <p>{ERROR_404_DESCRIPTION.FIRST_PART}</p>
-                <p>{ERROR_404_DESCRIPTION.SECOND_PART}</p>
+                <p>{DESCRIPTION}</p>
                 <NavLink to={PUBLIC_ROUTES.ABOUT_US.FULL} className="link-to-main">
                     <div className="link-block">
-                        <span className="link-title">{ERROR_404_GO_BACK}</span>
+                        <span className="link-title">{GO_BACK_BUTTON}</span>
                         <img src={ArrowIcon} alt="" />
                     </div>
                 </NavLink>

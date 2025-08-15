4223a79ef57eec9ac9858c24cf8dd40e05c7b7fd
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-08-16 00:53:13 +0300

    Add useInfiniteScroll hook

---
+++ src/hooks/common/use-infinite-scroll/useInfiniteScroll.test.tsx
@@ -0,0 +1,41 @@
+﻿import { renderHook, act } from '@testing-library/react';
+import { useInfiniteScroll, UseInfiniteScrollProps } from './useInfiniteScroll';
+import React from "react";
+
+describe('useInfiniteScroll', () => {
+    const createScrollEvent = (overrides: Partial<HTMLDivElement>): React.UIEvent<HTMLDivElement> =>
+        ({
+            currentTarget: {
+                scrollHeight: 100,
+                clientHeight: 50,
+                scrollTop: 45, // default close to bottom
+                ...overrides,
+            },
+        }) as unknown as React.UIEvent<HTMLDivElement>;
+
+    it('calls onReachBottom when scrolled to bottom', () => {
+        const onReachBottom = jest.fn();
+        const { result } = renderHook((props: UseInfiniteScrollProps) => useInfiniteScroll(props), {
+            initialProps: { onReachBottom } as UseInfiniteScrollProps,
+        });
+
+        act(() => {
+            result.current(createScrollEvent({ scrollTop: 45 }));
+        });
+
+        expect(onReachBottom).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call onReachBottom when disabled', () => {
+        const onReachBottom = jest.fn();
+        const { result } = renderHook((props: UseInfiniteScrollProps) => useInfiniteScroll(props), {
+            initialProps: { onReachBottom, disabledWhen: true } as UseInfiniteScrollProps,
+        });
+
+        act(() => {
+            result.current(createScrollEvent({ scrollTop: 45 }));
+        });
+
+        expect(onReachBottom).not.toHaveBeenCalled();
+    });
+});
---
+++ src/hooks/common/use-infinite-scroll/useInfiniteScroll.tsx
@@ -0,0 +1,28 @@
+﻿import React, { useCallback } from 'react';
+
+export const DEFAULT_BOTTOM_REACH_THRESHOLD_IN_PIXELS = 5;
+
+export interface UseInfiniteScrollProps {
+    onReachBottom: () => void;
+    disabledWhen?: boolean;
+    bottomReachThresholdInPixels?: number;
+}
+
+export const useInfiniteScroll = ({
+    onReachBottom,
+    disabledWhen = false,
+    bottomReachThresholdInPixels = DEFAULT_BOTTOM_REACH_THRESHOLD_IN_PIXELS,
+}: UseInfiniteScrollProps) => {
+    return useCallback(
+        (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+            const target = event.currentTarget;
+            const isAtBottom =
+                target.scrollHeight - target.scrollTop - target.clientHeight <= bottomReachThresholdInPixels;
+
+            if (isAtBottom && !disabledWhen) {
+                onReachBottom();
+            }
+        },
+        [onReachBottom, disabledWhen, bottomReachThresholdInPixels],
+    );
+};

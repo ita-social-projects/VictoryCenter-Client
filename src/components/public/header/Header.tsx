8b005bed88d2ae1fe897ab39e84b8b15d9d0d067
Rominos7 <Rominos7@gmail.com>
2025-08-25 20:15:29 +0300

    add localization support for header

--- src/components/public/header/Header.tsx
+++ src/components/public/header/Header.tsx
@@ -3,11 +3,16 @@ import './Header.scss';
 import { Link } from 'react-router';
 import { ReactComponent as VictoryCenterLogo } from '../../../assets/icons/logo-with-text.svg';
 import { PUBLIC_ROUTES } from '../../../const/public/routes';
-import { ABOUT_US, CONTACT_US, DONATE, HOW_TO_SUPPORT, PROGRAMS, REPORTING } from '../../../const/public/header';
+// import { ABOUT_US, CONTACT_US, DONATE, HOW_TO_SUPPORT, PROGRAMS, REPORTING } from '../../../const/public/header';
 import { ReactComponent as BurgerIcon } from '../../../assets/icons/burger.svg';
+import { useTranslation } from 'react-i18next';
+import '../../../i18n';
+import { LanguageSwitcher } from '../language-switcher/LanguageSwitcher';
 
 export const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+    const { t } = useTranslation('header');
 
     const toggleMenu = () => {
         setIsMenuOpen((prev: boolean) => !prev);
@@ -27,25 +32,27 @@ export const Header = () => {
                 </Link>
             </div>
 
+            <LanguageSwitcher />
+
             <div className="linkContainer">
                 <nav>
-                    <Link to={PUBLIC_ROUTES.ABOUT_US.FULL}>{ABOUT_US}</Link>
-                    <Link to={PUBLIC_ROUTES.PROGRAMS.FULL}>{PROGRAMS}</Link>
+                    <Link to={PUBLIC_ROUTES.ABOUT_US.FULL}>{t('ABOUT_US')}</Link>
+                    <Link to={PUBLIC_ROUTES.PROGRAMS.FULL}>{t('PROGRAMS')}</Link>
                     <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
-                        {REPORTING}
+                        {t('REPORTING')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
-                        {HOW_TO_SUPPORT}
+                        {t('HOW_TO_SUPPORT')}
                     </Link>
                 </nav>
             </div>
 
             <div className="buttonContainer">
                 <button className="contactUsButton" onClick={onContactUsClick}>
-                    {CONTACT_US}
+                    {t('CONTACT_US')}
                 </button>
                 <Link to={PUBLIC_ROUTES.DONATE.FULL} className="button donateButton">
-                    {DONATE}
+                    {t('DONATE')}
                 </Link>
                 <button onClick={toggleMenu} className="burgerMenuIcon">
                     <BurgerIcon />
@@ -54,16 +61,16 @@ export const Header = () => {
             {isMenuOpen && (
                 <div className="mobileMenu">
                     <Link to={PUBLIC_ROUTES.ABOUT_US.FULL} onClick={toggleMenu}>
-                        {ABOUT_US}
+                        {t('ABOUT_US')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.PROGRAMS.FULL} onClick={toggleMenu}>
-                        {PROGRAMS}
+                        {t('PROGRAMS')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.MOCK.FULL} onClick={toggleMenu} className="disable">
-                        {REPORTING}
+                        {t('REPORTING')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.MOCK.FULL} onClick={toggleMenu} className="disable">
-                        {HOW_TO_SUPPORT}
+                        {t('HOW_TO_SUPPORT')}
                     </Link>
                 </div>
             )}

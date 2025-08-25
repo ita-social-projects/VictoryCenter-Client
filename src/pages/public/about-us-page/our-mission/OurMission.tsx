33ff4f30dca3dae32f9eb745779ae8ee9d7f2f38
Rominos7 <Rominos7@gmail.com>
2025-08-25 20:15:47 +0300

    add localization support for abour us page sections

--- src/pages/public/about-us-page/our-mission/OurMission.tsx
+++ src/pages/public/about-us-page/our-mission/OurMission.tsx
@@ -1,20 +1,24 @@
 import './OurMission.scss';
 import { NavLink } from 'react-router';
 import ArrowIcon from '../../../../assets/icons/arrow-up-right.svg';
-import { ABOUT_US_DATA } from '../../../../const/public/about-us-page';
+// import { ABOUT_US_DATA } from '../../../../const/public/about-us-page';
 import { PUBLIC_ROUTES } from '../../../../const/public/routes';
 import { ScrollableFrame } from './scrollable-frame/ScrollableFrame';
+import { useTranslation } from 'react-i18next';
+import '../../../../i18n';
 
 export const OurMission = () => {
+    const { t } = useTranslation('aboutUsPage');
+
     return (
         <div className="our-mission-block">
             <div className="what-we-do">
-                <h2 className="mission-title">{ABOUT_US_DATA.WHAT_WE_DO}</h2>
+                <h2 className="mission-title">{t('WHAT_WE_DO')}</h2>
                 <div className="details-block">
-                    <p className="mission-details">{ABOUT_US_DATA.WHAT_WE_DO_DETAILS}</p>
+                    <p className="mission-details">{t('WHAT_WE_DO_DETAILS')}</p>
                     <NavLink to={PUBLIC_ROUTES.PROGRAMS.FULL} className="link-to-programs">
                         <div className="link-block">
-                            <span className="link-title">{ABOUT_US_DATA.GO_TO_PROGRAMS}</span>
+                            <span className="link-title">{t('GO_TO_PROGRAMS')}</span>
                             <img src={ArrowIcon} alt="" />
                         </div>
                     </NavLink>
--- src/pages/public/about-us-page/support-section/SupportSection.tsx
+++ src/pages/public/about-us-page/support-section/SupportSection.tsx
@@ -1,14 +1,18 @@
 import './SupportSection.scss';
 import { ABOUT_US_DATA } from '../../../../const/public/about-us-page';
+import { useTranslation } from 'react-i18next';
+import '../../../../i18n';
 
 export const SupportSection = () => {
+    const { t } = useTranslation('aboutUsPage');
+
     return (
         <div className="support-block">
-            <h2 className="support-title">{ABOUT_US_DATA.SUPPORT_TITLE}</h2>
+            <h2 className="support-title">{t('SUPPORT_TITLE')}</h2>
             {ABOUT_US_DATA.SUPPORT_DATA.map(({ IMG, ALT, DESCRIPTION }, index) => (
                 <div key={`${ALT}-${index}`} className="support-card">
                     <img src={IMG} alt={ALT} />
-                    <p className="support-description">{DESCRIPTION}</p>
+                    <p className="support-description">{t(`SUPPORT_DATA[${index}].DESCRIPTION`)}</p>
                 </div>
             ))}
         </div>

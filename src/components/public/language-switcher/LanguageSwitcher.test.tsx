c7cafdb30346d46153c66932f4a79db8f8db610e
Ilbko <ilbkopro60000@gmail.com>
2025-09-25 10:52:24 +0300

    lint

--- src/components/public/language-switcher/LanguageSwitcher.test.tsx
+++ src/components/public/language-switcher/LanguageSwitcher.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { LanguageSwitcher } from './LanguageSwitcher';
 import { languages } from '../../../const/common/languages';
 import i18n from '../../../i18n';
--- src/types/common/i18next.d.ts
+++ src/types/common/i18next.d.ts
@@ -2,10 +2,10 @@ import 'i18next';
 import aboutUsPage from '../../locales/uk/about-us.json';
 
 declare module 'i18next' {
-  interface CustomTypeOptions {
-    // inferring types automatically (such as arrays of objects)
-    resources: {
-      aboutUsPage: typeof aboutUsPage;
-    };
-  }
+    interface CustomTypeOptions {
+        // inferring types automatically (such as arrays of objects)
+        resources: {
+            aboutUsPage: typeof aboutUsPage;
+        };
+    }
 }

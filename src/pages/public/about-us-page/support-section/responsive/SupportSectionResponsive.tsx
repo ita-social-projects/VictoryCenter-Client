a56b32dcdf26643aa99781db716c7daac8e2bce4
Sviatoslav Dosiak <dosyaksvyatoslaw57@gmail.com>
2025-10-07 01:39:13 +0300

    add logic to responsive support section

--- src/pages/public/about-us-page/support-section/responsive/SupportSectionResponsive.tsx
+++ src/pages/public/about-us-page/support-section/responsive/SupportSectionResponsive.tsx
@@ -1,6 +1,41 @@
 import { ABOUT_US_DATA } from '../../../../../const/public/about-us-page';
+import { AboutUsContent } from '../../../../../types/public/about-us-page';
 
-export const SupportSectionResponsive = () => {
+export interface SupportSectionResponsiveProps {
+    content: AboutUsContent[] | null;
+}
+
+export const SupportSectionResponsive = ({ content }: SupportSectionResponsiveProps) => {
+    if (!content) return null;
+
+    const renderColumn = (side: 'left' | 'right') => {
+        const isLeft = side === 'left';
+
+        return (
+            <div className={`support-col ${side}`}>
+                {content
+                    .filter((_, index) => index % 2 === (isLeft ? 0 : 1))
+                    .map((item, columnIndex) => {
+                        const generalIndex = isLeft ? columnIndex * 2 : columnIndex * 2 + 1;
+
+                        const imageUrl = item.image?.url ?? ABOUT_US_DATA.SUPPORT_DATA[generalIndex].IMG;
+                        const altText = ABOUT_US_DATA.SUPPORT_DATA[generalIndex].ALT;
+                        const description = item.description;
+
+                        return (
+                            <div
+                                key={`${altText}-${side}-${columnIndex}`}
+                                className={`support-card card-${columnIndex + 1}`}
+                            >
+                                <img src={imageUrl} alt={altText} />
+                                <p className="support-description">{description}</p>
+                            </div>
+                        );
+                    })}
+            </div>
+        );
+    };
+
     return (
         <div className="support-block">
             <div className="main-values-title">
@@ -8,26 +43,8 @@ export const SupportSectionResponsive = () => {
             </div>
 
             <div className="support-columns">
-                <div className="support-col left">
-                    {ABOUT_US_DATA.SUPPORT_DATA.filter((_, index) => index % 2 === 0).map(
-                        ({ IMG, ALT, DESCRIPTION }, index) => (
-                            <div key={`${ALT}-left-${index}`} className={`support-card card-${index + 1}`}>
-                                <img src={IMG} alt={ALT} />
-                                <p className="support-description">{DESCRIPTION}</p>
-                            </div>
-                        ),
-                    )}
-                </div>
-                <div className="support-col right">
-                    {ABOUT_US_DATA.SUPPORT_DATA.filter((_, index) => index % 2 === 1).map(
-                        ({ IMG, ALT, DESCRIPTION }, index) => (
-                            <div key={`${ALT}-right-${index}`} className={`support-card card-${index + 1}`}>
-                                <img src={IMG} alt={ALT} />
-                                <p className="support-description">{DESCRIPTION}</p>
-                            </div>
-                        ),
-                    )}
-                </div>
+                {renderColumn('left')}
+                {renderColumn('right')}
             </div>
         </div>
     );

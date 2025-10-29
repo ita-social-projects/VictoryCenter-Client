842e1f1564db0077a3253fd6271ab678c9698c79
KobMiles <kobilinskiyn@gmail.com>
2025-10-29 19:03:32 +0200

    refactor, simplify JSX syntax in DonatePageAdmin component

--- src/pages/admin/donate/DonatePageAdmin.tsx
+++ src/pages/admin/donate/DonatePageAdmin.tsx
@@ -1,5 +1,5 @@
 import { DonatePageContent } from './components/donate-page-content/DonatePageContent';
 
 export const DonatePageAdmin = () => {
-    return <DonatePageContent></DonatePageContent>;
+    return <DonatePageContent />;
 };

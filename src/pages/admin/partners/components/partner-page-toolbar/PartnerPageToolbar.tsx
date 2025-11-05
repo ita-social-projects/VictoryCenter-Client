834dfa781db31ff426c1076ad7ba64da76ac78da
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-11-05 02:57:17 +0200

    Fix PartnerPageToolbar component

--- src/pages/admin/partners/components/partner-page-toolbar/PartnerPageToolbar.tsx
+++ src/pages/admin/partners/components/partner-page-toolbar/PartnerPageToolbar.tsx
@@ -4,18 +4,16 @@ import { ReactComponent as PlusIcon } from '../../../../../assets/icons/plus.svg
 import './PartnerPageToolbar.scss';
 
 export interface PartnerPageToolbarProps {
-    onAddPartner: () => void;
+    onAddSection: () => void;
 }
 
-export const PartnerPageToolbar = ({ onAddPartner }: PartnerPageToolbarProps) => {
+export const PartnerPageToolbar = ({ onAddSection }: PartnerPageToolbarProps) => {
     return (
-        <div className="toolbar par-toolbar" data-testid="partner-page-toolbar">
-            <div className="toolbar-actions">
-                <Button onClick={onAddPartner} buttonStyle="primary">
-                    {PARTNERS_TEXT.BUTTON.ADD_PARTNER_SECTION}
-                    <PlusIcon />
-                </Button>
-            </div>
+        <div className="partner-page-toolbar" data-testid="partner-page-toolbar">
+            <Button onClick={onAddSection} buttonStyle="primary">
+                {PARTNERS_TEXT.BUTTON.ADD_PARTNER_SECTION}
+                <PlusIcon />
+            </Button>
         </div>
     );
 };

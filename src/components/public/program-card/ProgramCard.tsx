e48dee46a81899d4208ccb0e72b36436fed6f8f3
pasterik <pomapaster@gmail.com>
2025-11-06 21:11:08 +0200

    Fix style in program section

--- src/components/public/program-card/ProgramCard.tsx
+++ src/components/public/program-card/ProgramCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PublishedProgramDto } from '../../../types/public/programs-page';
 import { ReactComponent as ArrowIcon } from '../../../assets/icons/arrow-up-right.svg';
 import './ProgramCardProgramsPage.scss';
@@ -16,8 +15,8 @@ export const ProgramCard = ({ program, className }: ProgramCardProps) => {
                 <div className="subtitle-info">
                     <div className="subtitle-content">
                         <div className="subtitle-link">
-                            <p className="program-subtitle">{program.name}</p>
-                            <h2 className="program-title">{program.categories.map((x) => x.name).join(', ')}</h2>
+                            <p className="program-subtitle">{program.categories.map((x) => x.name).join(', ')}</p>
+                            <h2 className="program-title">{program.name}</h2>
                         </div>
                         <div className="arrow-container">
                             <ArrowIcon className="arrow-icon" />

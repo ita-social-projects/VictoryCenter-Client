020374b755b00742dc1728a96b62272a36e80129
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-11-11 17:39:17 +0200

    Fix display "Додати варіант" button as disabled during creation
    - Remove conditional rendering of the add button
    - Add isAdding to disabled condition
    - Button now always visible but disabled when adding new option

--- src/pages/admin/donate/components/support-options/support-options-form/SupportOptionsForm.tsx
+++ src/pages/admin/donate/components/support-options/support-options-form/SupportOptionsForm.tsx
@@ -71,17 +71,15 @@ export const SupportOptionsForm = ({
                         <SupportOptionItem key="new" onSave={handleSaveNewOption} onCancel={() => setIsAdding(false)} />
                     )}
 
-                    {!isAdding && (
-                        <Button
-                            className="btn-add new"
-                            onClick={() => setIsAdding(true)}
-                            buttonStyle="primary"
-                            disabled={isLoading}
-                        >
-                            {DONATE_TEXT.SUPPORT_OPTIONS.ADD_NEW}
-                            <PlusIcon className="plus-icon" />
-                        </Button>
-                    )}
+                    <Button
+                        className="btn-add new"
+                        onClick={() => setIsAdding(true)}
+                        buttonStyle="primary"
+                        disabled={isLoading || isAdding}
+                    >
+                        {DONATE_TEXT.SUPPORT_OPTIONS.ADD_NEW}
+                        <PlusIcon className="plus-icon" />
+                    </Button>
                 </div>
             )}
         </div>

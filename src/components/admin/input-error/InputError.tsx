567d9c37b0b8a8d7125b338f804b7157a56278d0
Oleg Motruk Home <olegmotruk94@gmail.com>
2025-11-22 13:37:38 +0200

    refactored InputError

--- src/components/admin/input-error/InputError.tsx
+++ src/components/admin/input-error/InputError.tsx
@@ -1,9 +1,9 @@
-import './InputError.scss';
+import styles from './InputError.module.scss';
 
 export interface InputErrorProps {
     error?: string;
 }
 
 export const InputError = ({ error }: InputErrorProps) => {
-    return error ? <span className="input-error">{error}</span> : null;
+    return error ? <span className={styles['input-error']}>{error}</span> : null;
 };

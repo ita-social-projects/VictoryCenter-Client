20e49c212859f97cc39b8bd9efb1157a757c7077
Oleg Motruk Home <olegmotruk94@gmail.com>
2025-11-22 13:25:45 +0200

    refactored DragPreview

--- src/components/admin/drag-preview/DragPreview.tsx
+++ src/components/admin/drag-preview/DragPreview.tsx
@@ -1,29 +1,31 @@
 import { DragPreviewModel } from '../../../types/admin/common';
 import { ReactComponent as DragIcon } from '../../../assets/icons/dragger.svg';
 import React from 'react';
-import './DragPreview.scss';
+import styles from './DragPreview.module.scss';
+
 export interface DragPreviewProps<TEntity> {
     entity: TEntity;
     dragPreview: DragPreviewModel<TEntity>;
     renderEntityComponent: (entity: TEntity) => React.ReactNode;
 }
+
 export const DragPreview = <TEntity,>({ dragPreview, entity, renderEntityComponent }: DragPreviewProps<TEntity>) => {
     if (!dragPreview.visible || !dragPreview.item) return <></>;
 
     return (
         <div
-            className="drag-preview"
+            className={styles['drag-preview']}
             aria-hidden={true}
             style={{
                 left: dragPreview.x - 45,
                 top: dragPreview.y - 55,
             }}
         >
-            <div className="drag-preview-wrapper">
-                <div className="dragger">
+            <div className={styles['drag-preview-wrapper']}>
+                <div className={styles['dragger']}>
                     <DragIcon />
                 </div>
-                <div className="item-data">{renderEntityComponent(entity)}</div>
+                <div className={styles['item-data']}>{renderEntityComponent(entity)}</div>
             </div>
         </div>
     );

d326b3d2278db1abcbf5e0b58582ba6df93da764
Oleg Motruk Home <olegmotruk94@gmail.com>
2025-11-22 13:45:43 +0200

    refactored MultiSelectInput

--- src/components/admin/multi-select-input/MultiSelectInput.tsx
+++ src/components/admin/multi-select-input/MultiSelectInput.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as UncheckedBox } from '../../../assets/icons/chevron-un
 import { ReactComponent as ArrowDown } from '../../../assets/icons/chevron-down.svg';
 import { ReactComponent as ArrowUp } from '../../../assets/icons/chevron-up.svg';
 import classNames from 'classnames';
-import './MultiSelectInput.scss';
+import styles from './MultiSelectInput.module.scss';
 
 export interface MultiSelectInputProps<T> {
     id: string;
@@ -94,13 +94,13 @@ export const MultiSelectInput = <T,>({
     });
 
     return (
-        <div className="multiselect" ref={multiselectContainerRef}>
+        <div className={styles['multiselect']} ref={multiselectContainerRef}>
             <button
                 id={id}
                 type="button"
-                className={classNames('multiselect__placeholder-container', {
-                    'multiselect__placeholder-container--opened': isOpen,
-                    'multiselect__placeholder-container--disabled': disabled,
+                className={classNames(styles['multiselect__placeholder-container'], {
+                    [styles['multiselect__placeholder-container--opened']]: isOpen,
+                    [styles['multiselect__placeholder-container--disabled']]: disabled,
                 })}
                 onClick={toggleDropdown}
                 disabled={disabled}
@@ -108,25 +108,25 @@ export const MultiSelectInput = <T,>({
                 aria-expanded={isOpen}
             >
                 <div
-                    className={classNames('multiselect__placeholder', {
-                        'multiselect__placeholder--has-value': value.length > 0,
+                    className={classNames(styles['multiselect__placeholder'], {
+                        [styles['multiselect__placeholder--has-value']]: value.length > 0,
                     })}
                 >
-                    <div className="multiselect__placeholder-content">{displayLabel}</div>
-                    <div className="multiselect__chevron">{isOpen ? <ArrowUp /> : <ArrowDown />}</div>
+                    <div className={styles['multiselect__placeholder-content']}>{displayLabel}</div>
+                    <div className={styles['multiselect__chevron']}>{isOpen ? <ArrowUp /> : <ArrowDown />}</div>
                 </div>
             </button>
 
             {isOpen && !disabled && (
-                <div className="multiselect__options-container" role="listbox" aria-multiselectable="true">
+                <div className={styles['multiselect__options-container']} role="listbox" aria-multiselectable="true">
                     {options.map((option) => {
                         const selected = isSelected(option);
                         const optionId = getOptionId(option);
                         return (
                             <div
                                 key={optionId}
-                                className={classNames('multiselect__option', {
-                                    'multiselect__option--selected': selected,
+                                className={classNames(styles['multiselect__option'], {
+                                    [styles['multiselect__option--selected']]: selected,
                                 })}
                                 onClick={() => toggleOption(option)}
                                 onKeyDown={(e) => handleOptionKeyDown(e, option)}
@@ -134,10 +134,10 @@ export const MultiSelectInput = <T,>({
                                 aria-selected={selected}
                                 tabIndex={0}
                             >
-                                <div className="multiselect__option-checkbox">
+                                <div className={styles['multiselect__option-checkbox']}>
                                     {selected ? <CheckedBox /> : <UncheckedBox />}
                                 </div>
-                                <span className="multiselect__option-content">{getOptionName(option)}</span>
+                                <span className={styles['multiselect__option-content']}>{getOptionName(option)}</span>
                             </div>
                         );
                     })}

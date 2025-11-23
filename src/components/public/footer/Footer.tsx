94198a53d51db41b9678eea9742d1a5eb659b3a0
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2025-11-23 16:03:31 +0200

    refactored Footer

--- src/components/public/footer/Footer.tsx
+++ src/components/public/footer/Footer.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as PhoneIcon } from '../../../assets/icons/phone.svg';
 import { ReactComponent as MailIcon } from '../../../assets/icons/mail.svg';
 import { PUBLIC_ROUTES } from '../../../const/public/routes';
 import { useTranslation } from 'react-i18next';
-import './Footer.scss';
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
     const { t } = useTranslation('footer');
@@ -32,11 +32,11 @@ export const Footer = () => {
     };
 
     return (
-        <div className="footer-content">
-            <div className="main-block">
-                <div className="email-field">
-                    <span className="title">{t('STAY_UP_TO_DATE_WITH_THE_NEWS')}</span>
-                    <div className="input-block">
+        <div className={styles['footer-content']}>
+            <div className={styles['main-block']}>
+                <div className={styles['email-field']}>
+                    <span className={styles['title']}>{t('STAY_UP_TO_DATE_WITH_THE_NEWS')}</span>
+                    <div className={styles['input-block']}>
                         <input
                             type="email"
                             placeholder={t('ENTER_YOUR_EMAIL')}
@@ -44,76 +44,76 @@ export const Footer = () => {
                             onChange={(e) => setEmail(e.target.value)}
                             required
                         />
-                        <button onClick={handleClick} className="subscribe-btn" aria-label={t('SIGN_UP')}>
-                            <ArrowUpIcon className="arrow-up-icon" />
+                        <button onClick={handleClick} className={styles['subscribe-btn']} aria-label={t('SIGN_UP')}>
+                            <ArrowUpIcon className={styles['arrow-up-icon']} />
                         </button>
                     </div>
                 </div>
 
-                <div className="menu">
-                    <span className="title">{t('MENU')}</span>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                <div className={styles['menu']}>
+                    <span className={styles['title']}>{t('MENU')}</span>
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('REPORTING')}
                     </Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('STORE')}
                     </Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('HOW_TO_SUPPORT')}
                     </Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('STORIES_OF_VICTORIES')}
                     </Link>
                 </div>
 
-                <div className="about-us">
-                    <span className="title">{t('ABOUT_US')}</span>
+                <div className={styles['about-us']}>
+                    <span className={styles['title']}>{t('ABOUT_US')}</span>
                     <Link to={PUBLIC_ROUTES.ABOUT_US.FULL}>{t('ABOUT_US')}</Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('OUR_HISTORY')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.TEAM.FULL}>{t('OUR_TEAM')}</Link>
                     <Link to={PUBLIC_ROUTES.PARTNERS.FULL}>{t('PARTNERS')}</Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('EVENTS_AND_NEWS')}
                     </Link>
                 </div>
 
-                <div className="hippotherapy">
-                    <span className="title">{t('HIPPOTHERAPY')}</span>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                <div className={styles['hippotherapy']}>
+                    <span className={styles['title']}>{t('HIPPOTHERAPY')}</span>
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('WHAT_IS_HIPPOTHERAPY')}
                     </Link>
                     <Link to={PUBLIC_ROUTES.PROGRAMS.FULL}>{t('PROGRAMS')}</Link>
-                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className="disable">
+                    <Link to={PUBLIC_ROUTES.MOCK.FULL} className={styles['disable']}>
                         {t('PROGRAMS_SESSIONS')}
                     </Link>
                 </div>
             </div>
 
-            <div className="contact-block">
-                <div className="main-contacts">
-                    <button className="contact-item" onClick={() => copyToClipboard(t('EMAIL'))}>
+            <div className={styles['contact-block']}>
+                <div className={styles['main-contacts']}>
+                    <button className={styles['contact-item']} onClick={() => copyToClipboard(t('EMAIL'))}>
                         <MailIcon /> {t('EMAIL')}
                     </button>
-                    <button className="contact-item" onClick={() => copyToClipboard(t('PHONE'))}>
+                    <button className={styles['contact-item']} onClick={() => copyToClipboard(t('PHONE'))}>
                         <PhoneIcon /> {t('PHONE')}
                     </button>
                 </div>
-                <div className="social-media">
-                    <button className="contact-item" onClick={handleFacebookClick}>
+                <div className={styles['social-media']}>
+                    <button className={styles['contact-item']} onClick={handleFacebookClick}>
                         Facebook
                     </button>
-                    <button className="contact-item" onClick={handleTelegramClick}>
+                    <button className={styles['contact-item']} onClick={handleTelegramClick}>
                         Telegram
                     </button>
-                    <button className="contact-item" onClick={handleInstagramClick}>
+                    <button className={styles['contact-item']} onClick={handleInstagramClick}>
                         Instagram
                     </button>
                 </div>
             </div>
-            <div className="scrolling-text-wrapper">
-                <p className="scrolling-text">
+            <div className={styles['scrolling-text-wrapper']}>
+                <p className={styles['scrolling-text']}>
                     <span>{t('VICTORY_STARTS_WITH_YOU')} </span>
                     <span> {t('VICTORY_STARTS_WITH_YOU')}</span>
                 </p>

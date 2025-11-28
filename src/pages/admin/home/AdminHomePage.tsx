f088bb2f1d66b6719a05703e38098cac972befc4
Oleg Motruk Home <olegmotruk94@gmail.com>
2025-11-29 00:17:37 +0200

    refactored AdmingHome

--- src/pages/admin/home/AdminHomePage.tsx
+++ src/pages/admin/home/AdminHomePage.tsx
@@ -1,18 +1,18 @@
-import './AdminHomePage.scss';
+import styles from './AdminHomePage.module.scss';
 import { ReactComponent as ArrowIcon } from '../../../assets/icons/arrow-left.svg';
 import { MAIN_TEXT, SUB_TEXT, HINT_TEXT } from '../../../const/admin/home';
 
 export const AdminHomePage = () => {
     return (
-        <div className="admin-page-content">
-            <div className="admin-page-main-text">
+        <div className={styles['admin-page-content']}>
+            <div className={styles['admin-page-main-text']}>
                 <h1>{MAIN_TEXT}</h1>
             </div>
-            <div className="admin-page-sub-text">
+            <div className={styles['admin-page-sub-text']}>
                 <p>{SUB_TEXT}</p>
             </div>
-            <div className="admin-page-action-hint">
-                <ArrowIcon className="admin-page-action-hint-icon" />
+            <div className={styles['admin-page-action-hint']}>
+                <ArrowIcon className={styles['admin-page-action-hint-icon']} />
                 <p>{HINT_TEXT}</p>
             </div>
         </div>

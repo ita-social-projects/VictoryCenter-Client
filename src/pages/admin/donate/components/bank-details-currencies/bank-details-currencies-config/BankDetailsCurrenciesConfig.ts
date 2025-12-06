1bcf81e6fde1db48f915e782e9e28f1ef77f8789
denys-pavskyi <den.pavski@gmail.com>
2025-12-06 02:32:20 +0200

    Refactor/issue-1017: Refactored BankDetailsCurrenciesConfig to match new models

--- src/pages/admin/donate/components/bank-details-currencies/bank-details-currencies-config/BankDetailsCurrenciesConfig.ts
+++ src/pages/admin/donate/components/bank-details-currencies/bank-details-currencies-config/BankDetailsCurrenciesConfig.ts
@@ -1,5 +1,11 @@
 import { ForwardRefExoticComponent, RefAttributes } from 'react';
-import { UahBankDetailsType, ForeignBankDetailsType, BankCurrency } from '../../../../../../types/admin/donate';
+import {
+    UahBankDetailsDto,
+    CreateUahBankDetails,
+    UpdateUahBankDetails,
+    ForeignBankDetailsDto,
+    BankCurrency,
+} from '../../../../../../types/admin/donate';
 import { GenericFormProps, GenericFormRef } from '../../generic-form/GenericForm';
 import { FieldValues } from 'react-hook-form';
 import { CorrespondentBankDetailsForm, createBankDetailsForm } from '../bank-details-factory/BankDetailsFactory';
@@ -12,8 +18,8 @@ export interface BankDetailsConfig<TItem extends FieldValues> {
     createEmptyItem: (data: Partial<TItem>) => TItem;
     withCorrespondentBanks?: boolean;
     fetch: (client: AxiosInstance, currency?: BankCurrency) => Promise<TItem[]>;
-    create: (client: AxiosInstance, data: Omit<TItem, 'id'>) => Promise<TItem>;
-    update: (client: AxiosInstance, id: number, data: Partial<TItem>) => Promise<TItem>;
+    create: (client: AxiosInstance, data: any) => Promise<TItem>;
+    update: (client: AxiosInstance, id: number, data: any) => Promise<TItem>;
     delete: (client: AxiosInstance, id: number) => Promise<void>;
     correspondentForm?: ForwardRefExoticComponent<GenericFormProps<any> & RefAttributes<GenericFormRef>>;
     currency?: BankCurrency;
@@ -22,46 +28,46 @@ export interface BankDetailsConfig<TItem extends FieldValues> {
 export const bankDetailsConfig: Record<string, BankDetailsConfig<any>> = {
     UAH: {
         form: createBankDetailsForm('UAH'),
-        createEmptyItem: (data: Partial<UahBankDetailsType>) => ({ ...data }) as UahBankDetailsType,
+        createEmptyItem: (data: Partial<UahBankDetailsDto>) => ({ ...data }) as UahBankDetailsDto,
         fetch: (client: AxiosInstance) => BankDetailsUahApi.getAll(client),
-        create: (client: AxiosInstance, data: Omit<UahBankDetailsType, 'id'>) => BankDetailsUahApi.create(client, data),
-        update: (client: AxiosInstance, id: number, data: Partial<UahBankDetailsType>) =>
+        create: (client: AxiosInstance, data: CreateUahBankDetails) => BankDetailsUahApi.create(client, data),
+        update: (client: AxiosInstance, id: number, data: UpdateUahBankDetails) =>
             BankDetailsUahApi.update(client, id, data),
         delete: (client: AxiosInstance, id: number) => BankDetailsUahApi.delete(client, id),
     },
     USD: {
         form: createBankDetailsForm('USD'),
-        createEmptyItem: (data: Partial<ForeignBankDetailsType>) =>
+        createEmptyItem: (data: Partial<ForeignBankDetailsDto>) =>
             ({
                 ...data,
                 currency: BankCurrency.Usd,
                 correspondentBanks: data.correspondentBanks || [],
-            }) as ForeignBankDetailsType,
+            }) as ForeignBankDetailsDto,
         withCorrespondentBanks: true,
         correspondentForm: CorrespondentBankDetailsForm,
         currency: BankCurrency.Usd,
         fetch: (client: AxiosInstance) => ForeignBankDetailsApi.getAll(client, BankCurrency.Usd),
-        create: (client: AxiosInstance, data: Omit<ForeignBankDetailsType, 'id'>) =>
+        create: (client: AxiosInstance, data: Omit<ForeignBankDetailsDto, 'id'>) =>
             ForeignBankDetailsApi.create(client, { ...data, currency: BankCurrency.Usd }),
-        update: (client: AxiosInstance, id: number, data: Partial<ForeignBankDetailsType>) =>
+        update: (client: AxiosInstance, id: number, data: Partial<ForeignBankDetailsDto>) =>
             ForeignBankDetailsApi.update(client, id, data),
         delete: (client: AxiosInstance, id: number) => ForeignBankDetailsApi.delete(client, id),
     },
     EUR: {
         form: createBankDetailsForm('EUR'),
-        createEmptyItem: (data: Partial<ForeignBankDetailsType>) =>
+        createEmptyItem: (data: Partial<ForeignBankDetailsDto>) =>
             ({
                 ...data,
                 currency: BankCurrency.Eur,
                 correspondentBanks: data.correspondentBanks || [],
-            }) as ForeignBankDetailsType,
+            }) as ForeignBankDetailsDto,
         withCorrespondentBanks: true,
         correspondentForm: CorrespondentBankDetailsForm,
         currency: BankCurrency.Eur,
         fetch: (client: AxiosInstance) => ForeignBankDetailsApi.getAll(client, BankCurrency.Eur),
-        create: (client: AxiosInstance, data: Omit<ForeignBankDetailsType, 'id'>) =>
+        create: (client: AxiosInstance, data: Omit<ForeignBankDetailsDto, 'id'>) =>
             ForeignBankDetailsApi.create(client, { ...data, currency: BankCurrency.Eur }),
-        update: (client: AxiosInstance, id: number, data: Partial<ForeignBankDetailsType>) =>
+        update: (client: AxiosInstance, id: number, data: Partial<ForeignBankDetailsDto>) =>
             ForeignBankDetailsApi.update(client, id, data),
         delete: (client: AxiosInstance, id: number) => ForeignBankDetailsApi.delete(client, id),
     },

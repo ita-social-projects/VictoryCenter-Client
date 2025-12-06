3aded33ad2fb3c88fbeb956ef118c8f4b65ed80c
denys-pavskyi <den.pavski@gmail.com>
2025-12-06 02:29:36 +0200

    Fix/issue-1017: Refactored models in donate.ts to match models in API, separated Dtos from request models

--- src/types/admin/donate.ts
+++ src/types/admin/donate.ts
@@ -1,4 +1,11 @@
-export interface UahBankDetailsType {
+export enum BankCurrency {
+    Uah = 0,
+    Usd = 1,
+    Eur = 2,
+}
+
+// UAH Bank Details
+export interface UahBankDetailsDto {
     id: number;
     name: string;
     receiver: string;
@@ -7,31 +14,24 @@ export interface UahBankDetailsType {
     paymentPurpose: string;
 }
 
-export interface SupportOptionsType {
+export interface CreateUahBankDetails extends Omit<UahBankDetailsDto, 'id'> {}
+
+export interface UpdateUahBankDetails extends Omit<UahBankDetailsDto, 'id'> {}
+
+// Support Options
+export interface SupportOptionsDto {
     id: number;
     name: string;
     value: string;
     currency: BankCurrency;
 }
 
-export enum BankCurrency {
-    Uah = 0,
-    Usd = 1,
-    Eur = 2,
-}
+export interface CreateSupportOptionsDto extends Omit<SupportOptionsDto, 'id'> {}
 
-export interface CreateSupportOptionsRequest {
-    name: string;
-    value: string;
-    currency: BankCurrency;
-}
+export interface UpdateSupportOptionsDto extends Omit<SupportOptionsDto, 'id'> {}
 
-export interface UpdateSupportOptionsRequest {
-    name: string;
-    value: string;
-}
-
-export interface CorrespondentBankDetailsType {
+// Correspondent Bank Details
+export interface CorrespondentBankDetailsDto {
     id: number;
     name: string;
     swift: string;
@@ -40,7 +40,12 @@ export interface CorrespondentBankDetailsType {
     foreignBankDetailsId: number;
 }
 
-export interface ForeignBankDetailsType {
+export interface CreateCorrespondentBankDetails extends Omit<CorrespondentBankDetailsDto, 'id'> {}
+
+export interface UpdateCorrespondentBankDetails extends Omit<CorrespondentBankDetailsDto, 'id'> {}
+
+// Foreign Bank Details
+export interface ForeignBankDetailsDto {
     id: number;
     name: string;
     receiver: string;
@@ -48,5 +53,9 @@ export interface ForeignBankDetailsType {
     swift: string;
     address: string;
     currency: BankCurrency;
-    correspondentBanks: CorrespondentBankDetailsType[];
+    correspondentBanks: CorrespondentBankDetailsDto[];
 }
+
+export interface CreateForeignBankDetails extends Omit<ForeignBankDetailsDto, 'id' | 'correspondentBanks'> {}
+
+export interface UpdateForeignBankDetails extends Omit<ForeignBankDetailsDto, 'id' | 'correspondentBanks'> {}

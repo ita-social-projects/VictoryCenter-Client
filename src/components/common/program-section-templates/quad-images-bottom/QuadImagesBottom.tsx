306ea06ea43ce42be46a3be6c0b4219d442b5f11
denys-pavskyi <den.pavski@gmail.com>
2025-12-18 18:47:31 +0200

    feature/issue-861: Added QuadImagesBottom realization, added empty 2 and 3 template. Configured shared BaseSectionTemplate

--- src/components/common/program-section-templates/quad-images-bottom/QuadImagesBottom.tsx
+++ src/components/common/program-section-templates/quad-images-bottom/QuadImagesBottom.tsx
@@ -1,13 +1,48 @@
 import styles from './QuadImagesBottom.module.scss';
 
 export interface QuadImagesBottomProps {
-    // TODO: Add props for title, description, and images
+    title?: string;
+    description?: string;
+    image1?: string;
+    image2?: string;
+    image3?: string;
+    image4?: string;
 }
 
-export const QuadImagesBottom = (props: QuadImagesBottomProps) => {
+export const QuadImagesBottom = ({
+    title = '',
+    description = '',
+    image1 = '',
+    image2 = '',
+    image3 = '',
+    image4 = '',
+}: QuadImagesBottomProps) => {
     return (
         <div className={styles.container}>
-            <h2 className={styles.title}>Template 1</h2>
+            <div className={styles.topSection}>
+                <div className={styles.titleSection}>
+                    <h2 className={styles.title}>{title}</h2>
+                </div>
+                <div className={styles.descriptionSection}>
+                    <p className={styles.description}>{description}</p>
+                </div>
+            </div>
+            <div className={styles.bottomSection}>
+                <div className={styles.imagesGrid}>
+                    <div className={styles.imageWrapper1}>
+                        <img src={image1} alt="" className={styles.image} />
+                    </div>
+                    <div className={styles.imageWrapper2}>
+                        <img src={image2} alt="" className={styles.image} />
+                    </div>
+                    <div className={styles.imageWrapper3}>
+                        <img src={image3} alt="" className={styles.image} />
+                    </div>
+                    <div className={styles.imageWrapper4}>
+                        <img src={image4} alt="" className={styles.image} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

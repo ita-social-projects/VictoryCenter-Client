87b1041917b077e54e73eef5a30e22c13243eb55
snowflaki <112960665+snowflaki@users.noreply.github.com>
2025-12-29 19:22:29 +0200

    [REPORTS PAGE] add mock data and locales for cta button

--- src/pages/public/reports-page/ReportsPage.tsx
+++ src/pages/public/reports-page/ReportsPage.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { ReportsSection, SummarySection } from './components';
+import { CtaSection } from '@/components/public/cta';
+import { CTA_DATA } from '@/utils/mock-data/public/reports-page';
+import { PUBLIC_ROUTES } from '@/const/public/routes';
+import outroVideo from '@/assets/videos/public/partners-page/outro.mp4';
 
 export const ReportsPage: React.FC = () => {
+    const { t } = useTranslation('reportsPage');
+
     return (
         <>
             <SummarySection />
             <ReportsSection />
+            <CtaSection
+                title={CTA_DATA.title}
+                description={CTA_DATA.description}
+                mediaUrl={outroVideo}
+                buttons={[{ label: t('cta.buttonLabel'), href: PUBLIC_ROUTES.DONATE.FULL }]}
+            />
         </>
     );
 };
--- src/utils/mock-data/public/reports-page.ts
+++ src/utils/mock-data/public/reports-page.ts
@@ -1,13 +1,4 @@
-interface ReportsSummary {
-    collected: {
-        uah: number;
-        usd: number;
-    };
-    livesChanged: number;
-    exchangeRate?: number;
-}
-
-export const SUMMARY_DATA: ReportsSummary = {
+export const SUMMARY_DATA = {
     collected: {
         uah: 1249854.09,
         usd: 32890.9,
@@ -19,3 +10,9 @@ export const REPORTS_DATA = [
     { year: 2025, fileUrl: '/files/report-2025.pdf' },
     { year: 2024, fileUrl: '/files/report-2024.pdf' },
 ];
+
+export const CTA_DATA = {
+    title: 'Разом ми<br />можемо більше',
+    description:
+        'Ваш внесок — це не просто цифри в звітах. Це — врятовані історії, відновлені серця й нові надії. Долучайтеся до тих, хто змінює життя.',
+};

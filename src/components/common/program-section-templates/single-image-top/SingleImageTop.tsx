fded7ffd362888bb9e6bc5794580cd9deebb4f05
denys-pavskyi <den.pavski@gmail.com>
2025-12-29 19:22:38 +0200

    feature/issue-861: Added editable form for SingleImageTop, minor improvements

--- src/components/common/program-section-templates/single-image-top/SingleImageTop.tsx
+++ src/components/common/program-section-templates/single-image-top/SingleImageTop.tsx
@@ -1,4 +1,9 @@
+import cn from 'classnames';
 import { TitleDescriptionSection } from '../shared/title-description-section/TitleDescriptionSection';
+import { PhotoInputGroup } from '@/components/admin/input-groups/photo-input-group/PhotoInputGroup';
+import { ImageValues } from '@/types/common/image';
+import { COMMON_TEXT_ADMIN } from '@/const/admin/common';
+import { PROGRAM_SECTION_IMAGE_CONFIGS } from '@/const/admin/programs';
 import styles from './SingleImageTop.module.scss';
 
 export interface SingleImageTopProps {
@@ -6,6 +11,10 @@ export interface SingleImageTopProps {
     description?: string;
     image1?: string;
     isTemplate?: boolean;
+    isEditable?: boolean;
+    onTitleChange?: (value: string) => void;
+    onDescriptionChange?: (value: string) => void;
+    onImage1Change?: (file: ImageValues | null) => void;
 }
 
 export const SingleImageTop = ({
@@ -13,20 +22,65 @@ export const SingleImageTop = ({
     description = '',
     image1 = '',
     isTemplate = false,
+    isEditable = false,
+    onTitleChange,
+    onDescriptionChange,
+    onImage1Change,
 }: SingleImageTopProps) => {
     return (
-        <div className={styles.container}>
-            <div className={styles.topSection}>
-                <div className={styles.imageWrapper}>
-                    <img src={image1} alt="" className={styles.image} />
-                </div>
-            </div>
-            <TitleDescriptionSection
-                title={title}
-                description={description}
-                className={styles.bottomSection}
-                isTemplate={isTemplate}
-            />
+        <div
+            className={cn(styles.container, {
+                [styles.template]: isTemplate,
+                [styles.editable]: isEditable,
+            })}
+        >
+            {isEditable ? (
+                <>
+                    <div className={styles['top-section']}>
+                        <div className={styles['image-wrapper']}>
+                            <PhotoInputGroup
+                                id="section-image-1"
+                                name="section-image-1"
+                                value={null}
+                                onChange={onImage1Change || (() => {})}
+                                setError={() => {}}
+                                cropWidth={PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.cropWidth}
+                                cropHeight={PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.cropHeight}
+                                minWidth={PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.minWidth}
+                                minHeight={PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.minHeight}
+                                imageLabel={COMMON_TEXT_ADMIN.INPUT.DRAG_AND_DROP_FILE_HERE}
+                                imageSubText={COMMON_TEXT_ADMIN.INPUT.getImageSizeSubText(
+                                    PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.cropHeight,
+                                    PROGRAM_SECTION_IMAGE_CONFIGS.SINGLE_IMAGE_TOP.cropWidth,
+                                )}
+                                className="program-section-image-input"
+                            />
+                        </div>
+                    </div>
+                    <TitleDescriptionSection
+                        title={title}
+                        description={description}
+                        className={styles['bottom-section']}
+                        isEditable={true}
+                        onTitleChange={onTitleChange}
+                        onDescriptionChange={onDescriptionChange}
+                    />
+                </>
+            ) : (
+                <>
+                    <div className={styles['top-section']}>
+                        <div className={styles['image-wrapper']}>
+                            <img src={image1} alt="" className={styles.image} />
+                        </div>
+                    </div>
+                    <TitleDescriptionSection
+                        title={title}
+                        description={description}
+                        className={styles['bottom-section']}
+                        isTemplate={isTemplate}
+                    />
+                </>
+            )}
         </div>
     );
 };
--- src/utils/functions/render-program-section/renderProgramSection.tsx
+++ src/utils/functions/render-program-section/renderProgramSection.tsx
@@ -126,6 +126,10 @@ export const renderProgramSection = ({
                     description={data.description}
                     image1={data.image1}
                     isTemplate={isTemplate}
+                    isEditable={isEditable}
+                    onTitleChange={handlers?.onTitleChange}
+                    onDescriptionChange={handlers?.onDescriptionChange}
+                    onImage1Change={handlers?.onImage1Change}
                 />
             );
         case ProgramSectionTemplate.SingleImageRight:

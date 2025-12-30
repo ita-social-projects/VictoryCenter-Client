7cdda580c81fbbf25abe28d29eba7419ff007fa1
denys-pavskyi <den.pavski@gmail.com>
2025-12-30 04:26:00 +0200

    feature/issue-861: Added TitleDescriptionSection to TextOnly

--- src/components/common/program-section-templates/text-only/TextOnly.tsx
+++ src/components/common/program-section-templates/text-only/TextOnly.tsx
@@ -1,7 +1,5 @@
 import cn from 'classnames';
-import { InputWithCharacterLimitGroup } from '@/components/admin/input-groups/input-with-character-limit-group/InputWithCharacterLimitGroup';
-import { TextAreaWithCharacterLimitGroup } from '@/components/admin/input-groups/text-area-with-character-limit-group/TextAreaWithCharacterLimitGroup';
-import { PROGRAMS_TEXT } from '@/const/admin/programs';
+import { TitleDescriptionSection } from '@/components/common/program-section-templates/shared/title-description-section/TitleDescriptionSection';
 import styles from './TextOnly.module.scss';
 
 export interface TextOnlyProps {
@@ -21,54 +19,18 @@ export const TextOnly = ({
     onTitleChange,
     onDescriptionChange,
 }: TextOnlyProps) => {
-    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        onTitleChange?.(e.target.value);
-    };
-
-    const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        onDescriptionChange?.(e.target.value);
-    };
-
     return (
-        <div
+        <TitleDescriptionSection
+            title={title}
+            description={description}
+            isTemplate={isTemplate}
+            isEditable={isEditable}
+            onTitleChange={onTitleChange}
+            onDescriptionChange={onDescriptionChange}
             className={cn(styles.container, {
                 [styles.template]: isTemplate,
                 [styles.editable]: isEditable,
             })}
-        >
-            <div className={styles['title-section']}>
-                {isEditable ? (
-                    <InputWithCharacterLimitGroup
-                        label={PROGRAMS_TEXT.SECTION.FORM.TITLE.TEXT}
-                        isRequired={true}
-                        id="section-title"
-                        name="section-title"
-                        value={title}
-                        onChange={handleTitleChange}
-                        maxLength={60}
-                        placeholder={PROGRAMS_TEXT.SECTION.FORM.TITLE.PLACEHOLDER}
-                        className={styles['title-input']}
-                    />
-                ) : (
-                    <h2 className={styles.title}>{title}</h2>
-                )}
-            </div>
-            <div className={styles['description-section']}>
-                {isEditable ? (
-                    <TextAreaWithCharacterLimitGroup
-                        label={PROGRAMS_TEXT.SECTION.FORM.DESCRIPTION.TEXT}
-                        isRequired={true}
-                        id="section-description"
-                        name="section-description"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                        maxLength={600}
-                        rows={8}
-                    />
-                ) : (
-                    <p className={styles.description}>{description}</p>
-                )}
-            </div>
-        </div>
+        />
     );
 };

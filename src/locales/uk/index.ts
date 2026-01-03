7707035ffa8d4e819c81f523ba07345ae55d4182
Andriy Kravets <kraandriy14904@gmail.com>
2026-01-04 01:43:46 +0200

    Fix some errors

--- src/locales/uk/index.ts
+++ src/locales/uk/index.ts
@@ -4,6 +4,6 @@ import aboutUsPageUk from './about-us.json';
 import programsPageUk from './programs.json';
 import reportsPageUk from './reports.json';
 import globalUk from './global.json';
-import partnersPageUk from './partners.json';   
+import partnersPageUk from './partners.json';
 
 export { headerUk, footerUk, aboutUsPageUk, programsPageUk, reportsPageUk, globalUk, partnersPageUk };

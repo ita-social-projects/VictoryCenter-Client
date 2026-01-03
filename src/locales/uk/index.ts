9e694b9fe26606e3644958ead0ee0173156b514e
Andriy Kravets <kraandriy14904@gmail.com>
2026-01-03 17:01:14 +0200

    Fix some errors

--- src/locales/uk/index.ts
+++ src/locales/uk/index.ts
@@ -1,6 +1,6 @@
-export {default as headerEn } from './header.json';
-export {default as footerEn } from './footer.json';
-export {default as aboutUsPageEn } from './about-us.json';
+export {default as headerUk } from './header.json';
+export {default as footerUk } from './footer.json';
+export {default as aboutUsPageUk } from './about-us.json';
 export {default as programsPageUk } from './programs.json';
 export {default as reportsPageUk } from './reports.json';
 export {default as globalUk } from './global.json';

19e94a21d37f54e75d81168f6a8828aed3f2a148
denys-pavskyi <den.pavski@gmail.com>
2026-01-04 17:39:39 +0200

    refactor: shortened TitleDescriptionSection, reduced duplications

--- src/components/common/program-section-templates/text-only/TextOnly.tsx
+++ src/components/common/program-section-templates/text-only/TextOnly.tsx
@@ -4,29 +4,13 @@ import styles from './TextOnly.module.scss';
 export interface TextOnlyProps {
     title?: string;
     description?: string;
+    className?: string;
     isTemplate?: boolean;
     isEditable?: boolean;
     onTitleChange?: (value: string) => void;
     onDescriptionChange?: (value: string) => void;
 }
 
-export const TextOnly = ({
-    title = '',
-    description = '',
-    isTemplate = false,
-    isEditable = false,
-    onTitleChange,
-    onDescriptionChange,
-}: TextOnlyProps) => {
-    return (
-        <TitleDescriptionSection
-            title={title}
-            description={description}
-            isTemplate={isTemplate}
-            isEditable={isEditable}
-            onTitleChange={onTitleChange}
-            onDescriptionChange={onDescriptionChange}
-            templateStyles={styles}
-        />
-    );
+export const TextOnly = (props: TextOnlyProps) => {
+    return <TitleDescriptionSection {...props} templateStyles={styles} />;
 };

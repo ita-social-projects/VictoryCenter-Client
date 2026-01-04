3893be97879910230f2d0fa21e80790ba6f97161
Valentyn Humen <valentin34561@gmail.com>
2026-01-04 18:56:38 +0200

    add resolvedLanguage

--- src/hooks/common/use-locale/useLocale.ts
+++ src/hooks/common/use-locale/useLocale.ts
@@ -3,14 +3,15 @@ import { useTranslation } from 'react-i18next';
 export const useLocale = () => {
     const { i18n } = useTranslation();
 
+    const currentLang = i18n.resolvedLanguage || i18n.language;
     const changeLocaleLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
     };
 
     return {
-        currentLanguage: i18n.language,
+        currentLanguage: currentLang,
         changeLocaleLanguage,
-        isUk: i18n.language === 'uk',
-        isEn: i18n.language === 'en',
+        isUk: currentLang === 'uk',
+        isEn: currentLang === 'en',
     };
 };

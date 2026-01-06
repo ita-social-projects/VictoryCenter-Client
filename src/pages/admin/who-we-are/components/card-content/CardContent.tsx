47ae5b26b40c3078c13aeb56a74349cc4b4e3e6c
denys-pavskyi <den.pavski@gmail.com>
2026-01-06 21:43:11 +0200

    refactor: applied new version of TextTextAreaWithCharacterLimitGroup in CardContent, DescriptionSection, ImageBlockSection

--- src/pages/admin/who-we-are/components/card-content/CardContent.tsx
+++ src/pages/admin/who-we-are/components/card-content/CardContent.tsx
@@ -1,10 +1,10 @@
 import { COMMON_TEXT_ADMIN } from '@/const/admin/common';
-import { TextAreaWithCharacterLimit } from '@/components/admin/textarea-with-character-limit/TextAreaWithCharacterLimit';
 import React from 'react';
 import { ImageInput, ImageInputProps } from '@/components/admin/image-input/ImageInput';
 import { Content } from '@/types/admin/who-we-are';
 import { ImageValues } from '@/types/common/image';
 import { WHO_WE_ARE_TEXT } from '@/const/admin/who-we-are';
+import { TextAreaWithCharacterLimitGroup } from '@/components/admin/input-groups/text-area-with-character-limit-group/TextAreaWithCharacterLimitGroup';
 import './CardContent.scss';
 
 export interface CardContentProps {
@@ -60,8 +60,8 @@ export const CardContent = ({
             />
             {imageError && <p className="error">{imageError}</p>}
             <div className="card-content-description-wrapper">
-                <span className="card-content-description-wrapper-label">{COMMON_TEXT_ADMIN.TYPE.DESCRIPTION}</span>
-                <TextAreaWithCharacterLimit
+                <TextAreaWithCharacterLimitGroup
+                    label={COMMON_TEXT_ADMIN.TYPE.DESCRIPTION}
                     onChange={handleDescriptionChange}
                     value={content.description ?? ''}
                     maxLength={descriptionLimit}
@@ -69,8 +69,8 @@ export const CardContent = ({
                     id={content.id.toString()}
                     rows={rows}
                     onBlur={onDescriptionValidate}
+                    error={descriptionError ?? undefined}
                 />
-                {descriptionError && <p className="error">{descriptionError}</p>}
             </div>
         </div>
     );

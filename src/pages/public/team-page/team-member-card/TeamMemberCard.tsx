2a749255f09f5d235370a61afe4329741fa9fda5
Valentyn Humen <valentin34561@gmail.com>
2026-01-11 00:28:30 +0200

    integrate logic of the translated content on public team member page

--- src/pages/public/team-page/team-member-card/TeamMemberCard.tsx
+++ src/pages/public/team-page/team-member-card/TeamMemberCard.tsx
@@ -1,6 +1,8 @@
 import { MemberCard } from '@/types/public/team-page';
 import { ReactComponent as DefaultTeamMemberIcon } from '@/assets/icons/team-member-blank.svg';
 import { useState, useEffect } from 'react';
+import { useLocale } from '@/hooks/common/use-locale/useLocale';
+import { TeamMemberLocalizableFields } from '@/types/admin/team-members';
 
 interface TeamMemberProps {
     member: MemberCard;
@@ -8,10 +10,22 @@ interface TeamMemberProps {
 
 export const TeamMemberCard = ({ member }: TeamMemberProps) => {
     const [error, setError] = useState(false);
+    const { currentLanguage } = useLocale();
+    const [textFields, setTextFields] = useState<TeamMemberLocalizableFields>();
 
     useEffect(() => {
         setError(false);
     }, [member.photo]);
+
+    useEffect(() => {
+        const displayedLocalization = member.localizations?.find(
+            (loc) => loc.localizationInfoDto.code === currentLanguage,
+        );
+        setTextFields({
+            fullName: displayedLocalization?.fullName || member.name,
+            description: displayedLocalization?.description || member.role,
+        });
+    }, [currentLanguage, member]);
 
     return (
         <div className="team-member">
@@ -21,8 +35,8 @@ export const TeamMemberCard = ({ member }: TeamMemberProps) => {
                 <img src={member.photo} alt={member.name} className="member-photo" onError={() => setError(true)} />
             )}
             <div>
-                <p className="member-name">{member.name}</p>
-                <p className="member-role">{member.role}</p>
+                <p className="member-name">{textFields?.fullName}</p>
+                <p className="member-role">{textFields?.description}</p>
             </div>
         </div>
     );

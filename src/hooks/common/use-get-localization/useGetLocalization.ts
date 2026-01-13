0f9d74efbe5fd97873c0979b269c9dd4fd03223a
Valentyn Humen <valentin34561@gmail.com>
2026-01-14 00:24:57 +0200

    update hook and cover by tests

--- src/hooks/common/use-get-localization/useGetLocalization.ts
+++ src/hooks/common/use-get-localization/useGetLocalization.ts
@@ -2,15 +2,28 @@ import { useMemo } from 'react';
 import { useLocale } from '@/hooks/common/use-locale/useLocale';
 import { EntityLocalizationDto } from '@/types/common/language';
 
-export const useGetLocalization = <T>(localizations: EntityLocalizationDto[] | undefined, fallback: T): T => {
+export const useGetLocalization = <T extends object>(
+    localizations: EntityLocalizationDto[] | undefined,
+    fallback: T,
+): T => {
     const { currentLanguage } = useLocale();
 
     return useMemo(() => {
         const activeLoc = localizations?.find((loc) => loc.localizationInfoDto.code === currentLanguage);
 
+        if (!activeLoc) {
+            return fallback;
+        }
+
+        const {
+            localizationInfoDto: _localizationInfoDto,
+            translationStatus: _translationStatus,
+            ...localizableFields
+        } = activeLoc;
+
         return {
             ...fallback,
-            ...activeLoc,
+            ...localizableFields,
         };
     }, [currentLanguage, localizations, fallback]);
 };

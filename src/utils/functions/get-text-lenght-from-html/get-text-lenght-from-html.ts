fa9a3a783b828dfed5f05ee7c8fddf6e141cebe0
Rominos7 <Rominos7@gmail.com>
2026-01-15 01:40:29 +0200

    add script/style tag stripping to HTML utility functions

--- src/utils/functions/get-text-lenght-from-html/get-text-lenght-from-html.ts
+++ src/utils/functions/get-text-lenght-from-html/get-text-lenght-from-html.ts
@@ -1,6 +1,7 @@
 export const getTextLengthFromHtml = (html: string): number => {
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = html;
+    tempDiv.querySelectorAll('script, style').forEach((el) => el.remove());
     const text = tempDiv.innerText || tempDiv.textContent || '';
     return text.replace(/\n/g, '').length;
 };

f420a9b27407a798dca9faf120450eac93bbea0d
denys-pavskyi <den.pavski@gmail.com>
2026-01-15 04:05:35 +0200

    fix: minor fixes

--- src/pages/public/detailed-program-page/DetailedProgramPage.tsx
+++ src/pages/public/detailed-program-page/DetailedProgramPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './DetailedProgramPage.module.scss';
 import { DetailedProgramPageContent } from './components/detailed-program-page-content/DetailedProgramPageContent';
 
 export const DetailedProgramPage: React.FC = () => {

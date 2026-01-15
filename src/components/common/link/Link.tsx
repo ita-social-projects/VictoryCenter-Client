b7f259c896b6dc897b64338d6ba145fd9ec256de
Valentyn Humen <valentin34561@gmail.com>
2026-01-15 14:22:27 +0200

    add another suggestion from code rabbit

--- src/components/common/link/Link.tsx
+++ src/components/common/link/Link.tsx
@@ -5,8 +5,18 @@ import { DEFAULT_LOCALE } from '@/const/common/locales';
 export const Link = ({ to, children, ...props }: LinkProps) => {
     const { currentLanguage } = useLocale();
 
-    const getLocalizedPath = (path: string) => {
-        if (typeof path !== 'string') return path;
+    const getLocalizedPath = (
+        path: string | Partial<{ pathname: string; search?: string; hash?: string }>,
+    ): typeof path => {
+        if (typeof path !== 'string') {
+            if (path.pathname) {
+                return {
+                    ...path,
+                    pathname: getLocalizedPath(path.pathname) as string,
+                };
+            }
+            return path;
+        }
 
         if (currentLanguage === DEFAULT_LOCALE) return path;
 
@@ -16,7 +26,7 @@ export const Link = ({ to, children, ...props }: LinkProps) => {
     };
 
     return (
-        <RouterLink {...props} to={getLocalizedPath(to as string)}>
+        <RouterLink {...props} to={getLocalizedPath(to)}>
             {children}
         </RouterLink>
     );

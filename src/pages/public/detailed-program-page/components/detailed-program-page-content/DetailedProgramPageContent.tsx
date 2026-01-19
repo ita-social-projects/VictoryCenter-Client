3e30a76eb4cd1c97b0efb4beebe8346af998b4aa
denys-pavskyi <den.pavski@gmail.com>
2026-01-19 14:03:37 +0200

    refactor: Used BackgroundMedia in DetailedProgramPageContent, also some minor adjustments

--- src/pages/public/detailed-program-page/components/detailed-program-page-content/DetailedProgramPageContent.tsx
+++ src/pages/public/detailed-program-page/components/detailed-program-page-content/DetailedProgramPageContent.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { LinearProgress } from '@mui/material';
 import styles from './DetailedProgramPageContent.module.scss';
+import { BackgroundMedia } from '@/components/public/background-media/BackgroundMedia';
 import { ReactComponent as MapPin } from '@/assets/icons/map-pin.svg';
 import { ReactComponent as UsersRound } from '@/assets/icons/users-round.svg';
 import { ReactComponent as CalendarDays } from '@/assets/icons/calendar-days.svg';
 import { useProgramBySlug } from '@/hooks/common/use-get-program-by-slug/useGetProgramBySlug';
 import { InfoItem } from '../info-item/InfoItem';
-
-const getProgramImageUrl = (backgroundImage: { url: string } | { mimeType: string; base64: string }): string => {
-    return 'url' in backgroundImage
-        ? backgroundImage.url
-        : `data:${backgroundImage.mimeType};base64,${backgroundImage.base64}`;
-};
 
 export const DetailedProgramPageContent: React.FC = () => {
     const { slug } = useParams<{ slug: string }>();
@@ -41,38 +36,33 @@ export const DetailedProgramPageContent: React.FC = () => {
         <div className={styles['detailed-program-page-content']}>
             <div className={styles['background-section']}>
                 {program.backgroundImage && (
-                    <>
-                        <img
-                            src={getProgramImageUrl(program.backgroundImage)}
-                            className={styles['background-image']}
-                            alt={`${program.name} background`}
-                        />
-                        <div className={styles['overlay']} />
-                    </>
-                )}
-                <div className={styles['content-container']}>
-                    <div className={styles['left-section']}>
-                        <div>
-                            <h1 className={styles['program-name']}>{program.name}</h1>
-                            {(program.location || program.participantsCount) && (
-                                <div className={styles['program-info']}>
-                                    {program.location && <InfoItem icon={MapPin} text={program.location} />}
-                                    {program.participantsCount && (
-                                        <InfoItem icon={UsersRound} text={program.participantsCount} />
+                    <div className={styles['background-media-wrapper']}>
+                        <BackgroundMedia mediaUrl={program.backgroundImage.url} />
+                        <div className={styles['content-container']}>
+                            <div className={styles['left-section']}>
+                                <div>
+                                    <h1 className={styles['program-name']}>{program.name}</h1>
+                                    {(program.location || program.participantsCount) && (
+                                        <div className={styles['program-info']}>
+                                            {program.location && <InfoItem icon={MapPin} text={program.location} />}
+                                            {program.participantsCount && (
+                                                <InfoItem icon={UsersRound} text={program.participantsCount} />
+                                            )}
+                                        </div>
+                                    )}
+                                    {program.meetingsCount && (
+                                        <div className={styles['program-meetings']}>
+                                            <InfoItem icon={CalendarDays} text={program.meetingsCount} />
+                                        </div>
                                     )}
                                 </div>
-                            )}
-                            {program.meetingsCount && (
-                                <div className={styles['program-meetings']}>
-                                    <InfoItem icon={CalendarDays} text={program.meetingsCount} />
-                                </div>
-                            )}
+                            </div>
+                            <div className={styles['right-section']}>
+                                <p className={styles['description']}>{program.description}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles['right-section']}>
-                        <p className={styles['description']}>{program.description}</p>
-                    </div>
-                </div>
+                )}
             </div>
         </div>
     );

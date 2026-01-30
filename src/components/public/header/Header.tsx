312c7636999121ed1bbb5ca382fa68068817e6c0
Andriy Kravets <kraandriy14904@gmail.com>
2026-01-31 00:35:18 +0200

    Remove Store link because forgot do this in last task

--- src/components/public/header/Header.tsx
+++ src/components/public/header/Header.tsx
@@ -106,10 +106,6 @@ export const Header = () => {
                         </Link>
 
                         <Link to={PUBLIC_ROUTES.MOCK.FULL} onClick={toggleMenu} className="disable">
-                            {t('STORE')}
-                        </Link>
-
-                        <Link to={PUBLIC_ROUTES.MOCK.FULL} onClick={toggleMenu} className="disable">
                             {t('STORIES_OF_VICTORIES')}
                         </Link>
 

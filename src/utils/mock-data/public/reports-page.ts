bf87573dde5147e76c426e1896fc5ebb74364fa8
snowflaki <112960665+snowflaki@users.noreply.github.com>
2026-01-31 03:31:45 +0200

    [REPORTS PAGE] extend mock data

--- src/utils/mock-data/public/reports-page.ts
+++ src/utils/mock-data/public/reports-page.ts
@@ -16,3 +16,45 @@ export const CTA_DATA = {
     description:
         'Ваш внесок — це не просто цифри в звітах. Це — врятовані історії, відновлені серця й нові надії. Долучайтеся до тих, хто змінює життя.',
 };
+
+export const EXPENSES_DATA = {
+    items: [
+        { label: 'Інші', amount: 2270, percent: 0.2 },
+        { label: 'Комунікація та партнерства', amount: 103485, percent: 9.3 },
+        { label: 'Операційні', amount: 290955, percent: 26.2 },
+        { label: 'Програмні', amount: 443042, percent: 64.3 },
+    ],
+};
+
+export const FUNDING_DATA = {
+    items: [
+        { amount: 100000, label: 'Підтримка від бізнесу' },
+        { amount: 371964.52, label: 'Грантові кошти' },
+        { amount: 267889.57, label: 'Благодійні внески' },
+        {
+            amount: 700000,
+            label: 'Дуууже дооовга наааааааааааааааааааааааааааааааааааааааазва',
+        },
+    ],
+};
+
+export const PROGRAMS_ALLOCATION_DATA = {
+    items: [
+        {
+            label: 'для ветеранів',
+            amount: 356052,
+        },
+        {
+            label: 'для жінок',
+            amount: 152737,
+        },
+        {
+            label: 'для дітей',
+            amount: 205000,
+        },
+        {
+            label: 'інші',
+            amount: 35000,
+        },
+    ],
+};

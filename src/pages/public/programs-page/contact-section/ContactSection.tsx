dd6065950fdddb876ff1e0e9fc66bd11ddc19fd4
Rominos7 <Rominos7@gmail.com>
2026-02-07 22:52:42 +0200

    use CTA component

--- src/pages/public/programs-page/contact-section/ContactSection.tsx
+++ src/pages/public/programs-page/contact-section/ContactSection.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import background from '@/assets/videos/public/programs-page/contact_us_background.mp4';
 import { useTranslation } from 'react-i18next';
-import './ContactSection.scss';
+import { CtaSection } from '@/components/public/cta';
 
 export const ContactSection: React.FC = () => {
     const { t } = useTranslation('programsPage');
 
     return (
-        <div className="contact-us-block">
-            <video autoPlay muted loop playsInline aria-hidden="true">
-                <source src={background} type="video/mp4" />
-            </video>
-            <div className="contact-us-info">
-                <h2 className="contact-title">{t('PROGRAM_PROMPT')}</h2>
-                <div className="contact-button">
-                    <h4>{t('TEXT_US')}</h4>
-                    <button type="button" aria-label="Звʼязатись з нами">
-                        {t('CONTACT')}
-                    </button>
-                </div>
-            </div>
-        </div>
+        <CtaSection
+            title={t('PROGRAM_PROMPT')}
+            description={t('TEXT_US')}
+            mediaUrl={background}
+            buttons={[{ label: t('CONTACT') }]}
+        />
     );
 };

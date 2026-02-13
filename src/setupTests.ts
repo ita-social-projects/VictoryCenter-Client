78dbc2c57dbd12a84937c611526c4c40d248fa2c
Shribak Bohdan <shribakb@gmail.com>
2026-02-13 14:40:57 +0200

    feat/issue-1378: add mock setup for scrollIntoView

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -12,3 +12,6 @@ global.TextEncoder = TextEncoder as any;
 global.TextDecoder = TextDecoder as any;
 
 process.env.REACT_APP_BACKEND_URL = 'mocked-backend-url';
+
+HTMLElement.prototype.scrollIntoView = jest.fn();
+

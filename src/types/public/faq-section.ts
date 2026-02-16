49fe917717551af0ad1f11e6bde2afa98be6722a
VolodymyrS <volodymyr.sh.05@gmail.com>
2026-02-16 19:40:30 +0200

    feat: add localizations field to PublishedFaqQuestion interface

--- src/types/public/faq-section.ts
+++ src/types/public/faq-section.ts
@@ -1,5 +1,8 @@
+import { FaqLocalizationDto } from "@/types/admin/faq"
+
 export interface PublishedFaqQuestion {
     id: number;
     questionText: string;
     answerText: string;
+    localizations?: FaqLocalizationDto[];
 }

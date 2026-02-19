84ebe9603f29bf16f94b53ab055646f075d4c4eb
denys-pavskyi <den.pavski@gmail.com>
2026-02-19 22:02:25 +0200

    feature: Added FaqProgramSection section

---
+++ src/components/common/program-section-templates/faq-program-section/FaqProgramSection.tsx
@@ -0,0 +1,40 @@
+import { useTranslation } from 'react-i18next';
+import cn from 'classnames';
+import { FaqCard } from '@/components/public/faq-section/faq-card/FaqCard';
+import { PublishedFaqQuestion } from '@/types/public/faq-section';
+import { ProgramSectionMode } from '@/types/common/program-sections';
+import styles from './FaqProgramSection.module.scss';
+
+export interface FaqProgramSectionProps {
+    questions?: PublishedFaqQuestion[];
+    mode?: ProgramSectionMode;
+    title?: string;
+}
+
+export const FaqProgramSection = ({
+    questions = [],
+    mode = ProgramSectionMode.View,
+    title,
+}: FaqProgramSectionProps) => {
+    const { t } = useTranslation('programsPage');
+
+    const displayTitle = title || t('COMMON_QUESTIONS');
+    const isTemplate = mode === ProgramSectionMode.Template;
+
+    return (
+        <div
+            className={cn(styles['faq-section'], {
+                [styles['template']]: isTemplate,
+            })}
+        >
+            <div className={styles['faq-block']}>
+                <h2 className={styles['title']}>{displayTitle}</h2>
+                <div className={styles['questions-container']}>
+                    {questions.map((faq, index) => (
+                        <FaqCard key={faq.id || index} faq={faq} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};

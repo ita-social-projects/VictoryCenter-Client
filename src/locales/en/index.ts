fcd6cd8d08b7e250a526528619aab4acbb153692
Bohdan Shribak <shribakb@gmail.com>
2026-02-24 20:50:34 +0200

    Merge pull request #245 from feat/issue-810:quote_section_single_program
    Public page single program: add quote static section

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -2,9 +2,20 @@ import headerEn from './header.json';
 import footerEn from './footer.json';
 import aboutUsPageEn from './about-us.json';
 import programsPageEn from './programs.json';
+import detailedProgramPageEn from './detailed-program.json';
 import donateEn from './donate.json';
 import reportsPageEn from './reports.json';
 import globalEn from './global.json';
 import partnersPageEn from './partners.json';
 
-export { headerEn, footerEn, aboutUsPageEn, programsPageEn, donateEn, reportsPageEn, partnersPageEn, globalEn };
+export {
+    headerEn,
+    footerEn,
+    aboutUsPageEn,
+    programsPageEn,
+    detailedProgramPageEn,
+    donateEn,
+    reportsPageEn,
+    partnersPageEn,
+    globalEn,
+};

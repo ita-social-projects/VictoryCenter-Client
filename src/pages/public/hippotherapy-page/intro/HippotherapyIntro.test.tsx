9a0ba551e8f0f86e306fddb54b203a5a2364b8b3
Kateryna Maistrenko <silchencko@gmail.com>
2026-03-01 19:47:20 +0200

    format tests and css module

--- src/pages/public/hippotherapy-page/intro/HippotherapyIntro.test.tsx
+++ src/pages/public/hippotherapy-page/intro/HippotherapyIntro.test.tsx
@@ -3,18 +3,22 @@ import { render, screen } from '@testing-library/react';
 
 describe('HippotherapyIntro', () => {
     it('should render intro section with provided data', () => {
-        render(<HippotherapyIntro introData={{
-            imgURL: 'test-image.jpg',
-            imgAlternativeText: 'Test Image',
-            title: 'Test Title',
-            description: 'Test Description',
-        }} />);
+        render(
+            <HippotherapyIntro
+                introData={{
+                    imgURL: 'test-image.jpg',
+                    imgAlternativeText: 'Test Image',
+                    title: 'Test Title',
+                    description: 'Test Description',
+                }}
+            />,
+        );
 
         const image = screen.getByRole('img');
         expect(image).toBeInTheDocument();
         expect(image).toHaveAttribute('src', 'test-image.jpg');
         expect(image).toHaveAttribute('alt', 'Test Image');
         expect(screen.getByText('Test Title')).toBeInTheDocument();
-        expect(screen.getByText('Test Description')).toBeInTheDocument;
+        expect(screen.getByText('Test Description')).toBeInTheDocument();
     });
 });

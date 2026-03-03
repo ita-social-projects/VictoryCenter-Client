b30e1801dc57bac588589621f9e167dfb87196ce
Shribak Bohdan <shribakb@gmail.com>
2026-03-03 10:49:06 +0200

    feat/issue-1409: add api routes for funds and expenditures tab

--- src/const/common/api-routes/main-api.ts
+++ src/const/common/api-routes/main-api.ts
@@ -77,6 +77,11 @@ export const API_ROUTES = {
     REPORTS: {
         MEDIA_SETTINGS: 'Report/report',
         BASE: 'Report',
+        FUNDS_EXPENDITURES: {
+            SETTINGS: 'ReportFundsExpendituresSettings',
+            RECORDS: 'ReportFundsExpendituresRecords',
+            CATEGORIES: 'ReportFundsExpendituresCategories',
+        },
     },
     FAQ_LOCALIZATIONS: {
         BASE: 'FaqQuestionLocalizations',

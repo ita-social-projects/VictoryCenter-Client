0bcf2d3d44cbd854c25422745ca3aad733bf2dcf
Kateryna Maistrenko <silchencko@gmail.com>
2026-03-11 12:23:08 +0200

    trim white spaces in the Hippotherapy Slogan translations

--- src/pages/public/hippotherapy-page/slogan/SloganSection.tsx
+++ src/pages/public/hippotherapy-page/slogan/SloganSection.tsx
@@ -3,18 +3,20 @@ import styles from './SloganSection.module.scss';
 
 export const SloganSection = () => {
     const { t } = useTranslation('hippotherapy');
+    const yellowHighlightClass = `${styles.highlight} ${styles.yellow}`;
+    const blueHighlightClass = `${styles.highlight} ${styles.blue} ${styles['break-text']}`;
+    const spacedClass = `${styles['break-text']} ${styles.spaced}`;
 
     return (
         <section>
             <h1 className={styles.slogan} data-testid="slogan-section">
-                <span className={styles.highlight + ' ' + styles.yellow}>{t('SLOGAN.FIRST_HIGHLIGHT')}</span>
-                {t('SLOGAN.FIRST_TEXT')}
+                <span className={yellowHighlightClass}>{t('SLOGAN.FIRST_HIGHLIGHT')}</span>
+                {/* keep space for correct text breaking */}
+                <span className={spacedClass}> {t('SLOGAN.FIRST_TEXT')}</span>
                 <br />
-                <span>{t('SLOGAN.SECOND_TEXT')} </span>
-                <span className={styles.highlight + ' ' + styles.blue}>{t('SLOGAN.SECOND_HIGHLIGHT')} </span>
-                {/* add a space between span text */}
-                <span> </span>
-                <span className={styles.highlight + ' ' + styles.blue}>{t('SLOGAN.THIRD_HIGHLIGHT')}</span>
+                <span className={spacedClass}> {t('SLOGAN.SECOND_TEXT')} </span>
+                <span className={blueHighlightClass}>{t('SLOGAN.SECOND_HIGHLIGHT')} </span>
+                <span className={blueHighlightClass}>{t('SLOGAN.THIRD_HIGHLIGHT')}</span>
             </h1>
         </section>
     );

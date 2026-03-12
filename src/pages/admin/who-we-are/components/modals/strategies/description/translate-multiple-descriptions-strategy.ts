22a78d5b72fc7bb984958cfa5a5139632acf3704
VolodymyrS <volodymyr.sh.05@gmail.com>
2026-03-12 21:18:16 +0200

    feat: add localization retrieval in translateMultipleDescriptionsStrategy

--- src/pages/admin/who-we-are/components/modals/strategies/description/translate-multiple-descriptions-strategy.ts
+++ src/pages/admin/who-we-are/components/modals/strategies/description/translate-multiple-descriptions-strategy.ts
@@ -19,6 +19,10 @@ export const translateMultipleDescriptionsStrategy: WhoWeAreModalStrategy<Transl
 
         getInitialData: (section, language, isEditMode) => {
             if (!language) return null;
+            const localized = section.contents
+                .flatMap((content) => content.localizations ?? [])
+                .find((loc) => loc.language.code === language.code);
+
             let defaultImagesForSection: string[];
             switch (section.sectionType) {
                 case SectionType.WhoWeSupport:

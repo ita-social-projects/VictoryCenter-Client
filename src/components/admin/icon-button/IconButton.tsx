d4e5823792b81a8465aa3b9ef73dc0c1f63a4140
Vektor19 <viktor.komissarov@knu.ua>
2026-03-18 15:27:22 +0200

    refactor: rename HoverIconButtonProps to IconButtonProps for consistency

--- src/components/admin/icon-button/IconButton.tsx
+++ src/components/admin/icon-button/IconButton.tsx
@@ -1,12 +1,12 @@
 import cn from 'classnames';
 import styles from './IconButton.module.scss';
 
-export interface HoverIconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     DefaultIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
     FilledIcon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>> | null;
 }
 
-export const IconButton = ({ DefaultIcon, FilledIcon, className, ...props }: HoverIconButtonProps) => (
+export const IconButton = ({ DefaultIcon, FilledIcon, className, ...props }: IconButtonProps) => (
     <button {...props} className={cn(styles['icon-btn'], className)}>
         <DefaultIcon className={FilledIcon ? styles['icon-default'] : undefined} />
         {FilledIcon && <FilledIcon className={styles['icon-filled']} />}

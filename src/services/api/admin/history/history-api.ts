ee3bde4b8cbed14b310ff4c5a56696cd9c5fe738
Valentyn Humen <valentin34561@gmail.com>
2026-04-21 23:45:14 +0300

    handle api from backend and create interfaces for History

--- src/services/api/admin/history/history-api.ts
+++ src/services/api/admin/history/history-api.ts
@@ -1,4 +1,4 @@
-import { HistorySectionDto } from '@/types/common/history-sections';
+import { CreateUpdateHistorySectionDto, HistorySectionDto } from '@/types/common/history-sections';
 import { AxiosInstance } from 'axios';
 import { API_ROUTES } from '@/const/common/api-routes/main-api';
 
@@ -7,4 +7,10 @@ export const HistoryApi = {
         const response = await client.get<HistorySectionDto[]>(API_ROUTES.HISTORY.BASE);
         return response.data;
     },
+
+    syncSections: async (client: AxiosInstance, sections: CreateUpdateHistorySectionDto[]): Promise<HistorySectionDto[]> => {
+        const response = await client.put<CreateUpdateHistorySectionDto[]>(API_ROUTES.HISTORY.BASE, sections);
+        return response.data;
+    },
+
 };

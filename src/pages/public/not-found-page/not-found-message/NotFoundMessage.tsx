e71936b2fd49f995b795543854f822a0edd86393
piprock <yura.losov@gmail.com>
2026-08-19 00:30:53 +0300

    refactor: use localizePath utility for home route link

--- src/pages/public/not-found-page/not-found-message/NotFoundMessage.tsx
+++ src/pages/public/not-found-page/not-found-message/NotFoundMessage.tsx
@@ -1,13 +1,15 @@
 import styles from './NotFoundMessage.module.scss';
 import { ReactComponent as ArrowIcon } from '@/assets/icons/arrow-up-right.svg';
 import { Button } from '@/components/public/ui/button';
+import { PUBLIC_ROUTES } from '@/const/public/routes';
 import { useLocale } from '@/hooks/common/use-locale/useLocale';
+import { localizePath } from '@/utils/functions/localize-path/localize-path';
 import { useTranslation } from 'react-i18next';
 
 export const NotFoundMessage = () => {
     const { t } = useTranslation('notFoundPage');
     const { currentLanguage } = useLocale();
-    const homeHref = currentLanguage === 'en' ? '/en' : '/';
+    const homeHref = localizePath(PUBLIC_ROUTES.ROOT, currentLanguage);
 
     return (
         <div className={styles.root}>

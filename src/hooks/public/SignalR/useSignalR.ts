5afb1ba492669a5b2d7d9024110f5187ee55cce1
Urazovskyi Oleksandr <urazovskyi.sasha@gmail.com>
2026-08-25 07:36:20 +0300

    1) SignalR URL is taken from the environment variable now instead of being hardcoded 2) Pagination is now used to fetch pdf reports by portions instead of loading everything at once 3) 'mounted' flag inside of the 'ReportsSection' component was introduced to eliminate the chance of potential memory leaks 4) every possible event that updates the state of pdf reports is now being listened to separately in order to update the 'ReportsSection' component more efficiently 5) error handling and retrying logic of an initially not established connection was introduced in the 'useSignalR.ts' hook

--- src/hooks/public/SignalR/useSignalR.ts
+++ src/hooks/public/SignalR/useSignalR.ts
@@ -1,22 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 
 export const useSignalR = (url: string): HubConnection | null => {
     const [connection, setConnection] = useState<HubConnection | null>(null);
+    const isMounted = useRef(true);
 
     useEffect(() => {
+        isMounted.current = true;
+
         const newConnection = new HubConnectionBuilder().withUrl(url).withAutomaticReconnect().build();
 
-        newConnection
-            .start()
-            .then(() => {
-                setConnection(newConnection);
-            })
-            .catch((err: Error) => {
-                console.error('SignalR Connection Error: ', err);
-            });
+        let retryCount = 0;
+        const maxRetries = 5;
+
+        const startConnection = async () => {
+            try {
+                await newConnection.start();
+                if (isMounted.current) {
+                    setConnection(newConnection);
+                }
+            } catch (err) {
+                console.error(`SignalR Connection Error (Attempt ${retryCount + 1}): `, err);
+
+                if (retryCount < maxRetries && isMounted.current) {
+                    const timeout = Math.pow(2, retryCount) * 1000;
+                    retryCount++;
+                    setTimeout(startConnection, timeout);
+                }
+            }
+        };
+
+        startConnection();
 
         return () => {
+            isMounted.current = false;
             newConnection.stop();
         };
     }, [url]);

97a7a31e75ff73c5c0ccee0f5ddc79b7a1bd13ec
Ярослав Кучер <kucheryaroslav2@gmail.com>
2026-09-02 23:38:12 +0300

    feature: fix: Adding logic for closing the form from the keyboard. Replacing hardcoded strings with constant substitutions

--- src/pages/public/programs-page/contact-section/contact-form-pop-up/ContactFormPopUp.tsx
+++ src/pages/public/programs-page/contact-section/contact-form-pop-up/ContactFormPopUp.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import FocusLock from 'react-focus-lock';
 import { ContactFormCard } from '@/pages/public/contact-us/components/contact-form-card/ContactFormCard';
 import { ReactComponent as CrossIcon } from '@/assets/icons/cross.svg';
 import styles from './ContactFormPopUp.module.scss';
+import { CONTACT_US_PAGE_DATA } from '@/utils/mock-data/public/contact-us-page';
 
 interface ContactFormPopUpProps {
     isOpen: boolean;
@@ -14,15 +16,25 @@ export const ContactFormPopUp: React.FC<ContactFormPopUpProps> = ({ isOpen, onCl
     const isMouseDownOnOverlay = useRef(false);
 
     useEffect(() => {
+        const handleGlobalKeyDown = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                onClose();
+            }
+        };
+
         if (isOpen) {
             document.body.style.overflow = 'hidden';
+            document.addEventListener('keydown', handleGlobalKeyDown);
         } else {
             document.body.style.overflow = 'auto';
+            document.removeEventListener('keydown', handleGlobalKeyDown);
         }
+
         return () => {
             document.body.style.overflow = 'auto';
+            document.removeEventListener('keydown', handleGlobalKeyDown);
         };
-    }, [isOpen]);
+    }, [isOpen, onClose]);
 
     const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
         if (e.target === overlayRef.current) {
@@ -46,30 +58,33 @@ export const ContactFormPopUp: React.FC<ContactFormPopUpProps> = ({ isOpen, onCl
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
             data-testid="popup-overlay"
+            aria-hidden="true"
         >
-            <aside className={styles.popup} onClick={(e) => e.stopPropagation()}>
-                <button className={styles.closeBtn} onClick={onClose} aria-label="Закрити форму">
-                    <CrossIcon width="24" height="24" />
-                </button>
+            <FocusLock>
+                <aside role="presentation" className={styles.popup} onClick={(e) => e.stopPropagation()}>
+                    <button className={styles.closeBtn} onClick={onClose} aria-label="Закрити форму">
+                        <CrossIcon width="24" height="24" />
+                    </button>
 
-                <div className={styles.content}>
-                    <h2 className={styles.title}>
-                        МИ ЗАВЖДИ
-                        <br />
-                        ВІДКРИТІ ДЛЯ ВАС
-                    </h2>
+                    <div className={styles.content}>
+                        <h2 className={styles.title}>
+                            {CONTACT_US_PAGE_DATA.contactFormHeader1}
+                            <br />
+                            {CONTACT_US_PAGE_DATA.contactFormHeader2}
+                        </h2>
 
-                    <ContactFormCard
-                        isPopup={true}
-                        title="Контактна форма"
-                        namePlaceholder="Ваше ім'я"
-                        emailPlaceholder="E-mail"
-                        subjectPlaceholder="Тема звернення"
-                        messagePlaceholder="Напишіть ваше повідомлення"
-                        submitLabel="Надіслати"
-                    />
-                </div>
-            </aside>
+                        <ContactFormCard
+                            isPopup={true}
+                            title={CONTACT_US_PAGE_DATA.formLabel}
+                            namePlaceholder={CONTACT_US_PAGE_DATA.namePlaceholder}
+                            emailPlaceholder={CONTACT_US_PAGE_DATA.emailPlaceholder}
+                            subjectPlaceholder={CONTACT_US_PAGE_DATA.subjectPlaceholder}
+                            messagePlaceholder={CONTACT_US_PAGE_DATA.messagePlaceholder}
+                            submitLabel={CONTACT_US_PAGE_DATA.submitButton}
+                        />
+                    </div>
+                </aside>
+            </FocusLock>
         </div>
     );
 

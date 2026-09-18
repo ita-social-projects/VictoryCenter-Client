217a49d6a801ce560e8638e57c30cd5267559057
Maria Maksymenko <136785612+Maksikd62@users.noreply.github.com>
2026-09-18 15:42:24 +0300

    fix: show correct duplicate program category error

--- src/const/admin/reports.ts
+++ src/const/admin/reports.ts
@@ -371,6 +371,7 @@ export const PROGRAM_EXPENSES_TEXT = {
     },
     VALIDATION: {
         PROGRAM_UNIQUE: 'Категорія має бути унікальною',
+        PROGRAM_ALREADY_ADDED: 'Така категорія вже додана. Оберіть іншу категорію',
     },
     FILTER: {
         PROGRAMS_PLACEHOLDER: 'Програми',

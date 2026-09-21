2ce2ac0ed6a63842c2b876b6ffd056502a4f10ba
= <=>
2026-09-21 19:46:27 +0300

    replace events with event-news for types

--- src/types/admin/events-news.ts
+++ src/types/admin/events-news.ts
@@ -1,5 +1,42 @@
+import { Image, ImageValues } from '../common/image';
+import { VisibilityStatus } from './common';
+
 export interface EventsNews {
     resource: string;
     publishedAt: string;
     status: number;
+}
+
+export interface EventItemDto {
+    id: number;
+    resource: string;
+    publishedAt: string;
+    title: string;
+    description: string;
+    status: VisibilityStatus;
+    previewImage: Image | ImageValues | null;
+    backgroundImage: Image | ImageValues | null;
+}
+
+export type EventsErrorType = 'categories' | 'events' | 'search';
+
+export interface ErrorState {
+    message: string | null;
+    type: EventsErrorType | null;
+}
+
+// this is a test interface and most probably will need adjustments in the future
+export interface EventSearchItemData {
+    id: number;
+    name: string;
+    categories: string[];
+}
+
+// this is a test interface and most probably will need adjustments in the future
+export interface EventsLocalizableFields {
+    name: string;
+    description: string;
+    location: string;
+    participantsCount: string;
+    meetingsCount: string;
 }

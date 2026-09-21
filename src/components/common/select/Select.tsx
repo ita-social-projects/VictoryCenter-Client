bcd6f14e7d21fb089cd0d1af995446c62a2e3d3f
Sofia Fedoruk <sofiafedoruk2007@gmail.com>
2026-09-22 00:24:33 +0300

    fix: took away condition that was causing tests' failure

--- src/components/common/select/Select.tsx
+++ src/components/common/select/Select.tsx
@@ -68,8 +68,9 @@ export const Select = <TValue,>({
     }, [isOpen, containerRef]);
 
     const handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
-        const hasFocusLeftSelect =
-            containerRef.current && e.relatedTarget ? !containerRef.current.contains(e.relatedTarget as Node) : false;
+        const hasFocusLeftSelect = containerRef.current
+            ? !containerRef.current.contains(e.relatedTarget as Node)
+            : false;
 
         if (isOpen && hasFocusLeftSelect) {
             setIsOpen(false);
